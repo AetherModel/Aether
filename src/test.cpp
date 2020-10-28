9d58b79ccc11dde6759b4a5b1bb47267b3357668
Aaron Ridley <ridley@umich.edu>
2020-10-28 15:19:22 -0400

    FEAT: Added testing code and time unit test

---
+++ src/test.cpp
@@ -0,0 +1,22 @@
+// (c) 2020, the Aether Development Team (see doc/dev_team.md for members)
+// Full license can be found in License.md
+
+#include <iostream>
+
+#include "../include/time_conversion.h"
+
+int main() {
+
+  int iErr = 0;
+
+  // ------------------------------------------------------------
+  // Test time routines:
+  // ------------------------------------------------------------
+  
+  iErr = test_time_routines();
+  if (iErr == 0) std::cout << "Passed test_time_routines!\n";
+  else std::cout << "Failed test_time_routines!\n";
+  
+  return iErr;
+  
+}
