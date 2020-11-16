723e3f89f51e1d789f12f13bf9b09f9e2d8358c7
Aaron Ridley <ridley@umich.edu>
2020-11-16 15:50:55 -0500

    FEAT: Read EUV file and do EUV stuff

---
+++ include/euv.h
@@ -0,0 +1,82 @@
+// (c) 2020, the Aether Development Team (see doc/dev_team.md for members)
+// Full license can be found in License.md
+
+#ifndef AETHER_INCLUDE_EUV_H_
+#define AETHER_INCLUDE_EUV_H_
+
+#include <vector>
+#include <string>
+
+#include "inputs.h"
+#include "times.h"
+#include "indices.h"
+#include "planets.h"
+
+class Euv {
+
+public:
+
+  int nWavelengths;
+  int nLines;
+
+  struct waveinfotype {
+
+    std::string name;
+    std::string to;
+    std::string type;
+    std::string units;
+    std::string note;
+    std::vector<float> values;
+
+  };
+
+  std::vector<waveinfotype> waveinfo;
+
+  std::vector<float> wavelengths_short;
+  std::vector<float> wavelengths_long;
+  std::vector<float> wavelengths_energy;
+  std::vector<float> wavelengths_intensity_1au;
+  std::vector<float> wavelengths_intensity_top;
+
+  std::vector<float> euvac_f74113;
+  std::vector<float> euvac_afac;
+
+  // --------------------------------------------------------------------------
+  // Initialize EUV
+  // --------------------------------------------------------------------------
+
+  Euv(Inputs args);
+
+  // -------------------------------------------------------------------------
+  //
+  // -------------------------------------------------------------------------
+
+  int euvac(Times time, Indices indices, Inputs args);
+
+  // -------------------------------------------------------------------------
+  //
+  // -------------------------------------------------------------------------
+
+  int scale_from_1au(Planets planet, Times time, Inputs args);
+
+private:
+  
+  // --------------------------------------------------------------------------
+  // Read in the EUV file that describes all of the wavelengths and
+  // cross sections
+  // --------------------------------------------------------------------------
+
+  int read_file(Inputs args);
+
+  // --------------------------------------------------------------------------
+  //
+  // --------------------------------------------------------------------------
+
+  int slot_euv(std::string item,
+	       std::string item2,
+	       std::vector<float> &values,
+	       Inputs args);
+  
+};
+
+#endif // AETHER_INCLUDE_EUV_H_
---
+++ src/euv.cpp
@@ -0,0 +1,247 @@
+// (c) 2020, the Aether Development Team (see doc/dev_team.md for members)
+// Full license can be found in License.md
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream> 
+#include <iostream>
+
+#include "../include/constants.h"
+#include "../include/inputs.h"
+#include "../include/indices.h"
+#include "../include/euv.h"
+#include "../include/report.h"
+
+
+// -----------------------------------------------------------------------------
+// Initialize EUV
+// -----------------------------------------------------------------------------
+
+Euv::Euv(Inputs args) {
+  int iErr;
+  float ave;
+
+  iErr = 0;
+
+  // Read in the EUV file:
+  iErr = read_file(args);
+
+  if (!iErr) {
+    // Slot the short and long wavelengths into their arrays:
+    iErr = slot_euv("Long", "", wavelengths_long, args);
+    if (!iErr) iErr = slot_euv("Short", "", wavelengths_short, args);
+
+    // This means we found both long and short wavelengths:
+    if (!iErr) {
+      for (int iWave = 0; iWave < nWavelengths; iWave++) {
+	ave = (wavelengths_short[iWave] + wavelengths_long[iWave])/2.0;
+	wavelengths_energy.push_back(planck_constant *
+				     speed_light /
+				     (ave * atom));
+	// We simply want to initialize these vectors to make them the
+	// correct lenght:
+	wavelengths_intensity_1au.push_back(0.0);
+	wavelengths_intensity_top.push_back(0.0);
+      }
+    }
+
+    // Slot the EUVAC model coefficients:
+    if (args.get_euv_model() == "euvac") {
+      iErr = slot_euv("F74113", "", euvac_f74113, args);
+      iErr = slot_euv("AFAC", "", euvac_afac, args);
+    }
+  }
+}
+
+// ---------------------------------------------------------------------------
+// Read in the EUV file that describes all of the wavelengths and
+// cross sections
+// ---------------------------------------------------------------------------
+
+int Euv::read_file(Inputs args) {
+
+  waveinfotype tmp;
+  std::string line, col;
+  float mulfac;
+  std::ifstream infile_ptr;
+  int iErr = 0;
+
+  std::cout << "EUV File : " << args.get_euv_file() << "\n";
+
+  infile_ptr.open(args.get_euv_file());
+
+  if (!infile_ptr.is_open()) {
+    std::cout << "Could not open euv file!\n";
+    iErr = 1;
+  } else {
+
+    nLines = 0;
+
+    if (infile_ptr.good()) {
+
+      int IsFirstTime = 1;
+
+      while (getline(infile_ptr,line)) {
+
+	if (args.iVerbose > 5) std::cout << line << "\n";
+	std::stringstream ss(line);
+
+	// This is just to count the number of wavelengths.
+	// We assume that all of the lines have the same number of wavelengths.
+	if (IsFirstTime) {
+	  std::stringstream ssdummy(line);
+	  nWavelengths = 0;
+	  while (getline(ssdummy, col, ',')) {
+	    nWavelengths++;
+	  }
+	  // There are 6 extra items in each line:
+	  nWavelengths -= 6;
+	}
+
+	getline(ss, tmp.name, ',');
+	if (args.iVerbose > 5) std::cout << tmp.name << "\n";
+	getline(ss, tmp.to, ',');
+	getline(ss, tmp.type, ',');
+	getline(ss, col, ',');
+	mulfac = stof(col);
+	getline(ss, tmp.units, ',');
+
+	for (int iWavelength=0; iWavelength < nWavelengths; iWavelength++) {
+	  getline(ss, col, ',');
+	  if (IsFirstTime) tmp.values.push_back(stof(col) * mulfac);
+	  else tmp.values[iWavelength] = stof(col) * mulfac;
+	}
+	getline(ss, tmp.note, ',');
+
+	waveinfo.push_back(tmp);
+	nLines++;
+	IsFirstTime = 0;
+
+      }
+
+    } else {
+
+      iErr = 1;
+
+    }
+
+    infile_ptr.close();
+
+  }
+
+  return iErr;
+
+}
+
+// ---------------------------------------------------------------------------
+//
+// ---------------------------------------------------------------------------
+
+int Euv::slot_euv(std::string item,
+		  std::string item2,
+		  std::vector<float> &values,
+		  Inputs args) {
+
+  int iErr = 0;
+  int iLine;
+  int IgnoreItem2 = 0;
+
+  if (args.iVerbose > 3)
+    std::cout << "in slot_euv:" << item << ";" << item2 << "; nLines : " << nLines << "\n";
+
+  if (item2 == "") IgnoreItem2 = 1;
+
+  // Find item to move:
+  for (iLine=0; iLine < nLines ; iLine++) {
+    if (waveinfo[iLine].name == item) {
+      if (IgnoreItem2) break;
+      else if (waveinfo[iLine].to == item2) break;
+    }
+  }
+
+  if (iLine >= nLines) {
+    iErr = 1;
+  } else {
+
+    if (args.iVerbose > 2) {
+      std::cout << "Found : " << waveinfo[iLine].name;
+      if (!IgnoreItem2) std::cout << " with " << waveinfo[iLine].to;
+      std::cout << "\n";
+    }
+
+    // Move values into the output array (values):
+    for (int iWavelength=0; iWavelength < nWavelengths; iWavelength++) {
+      values.push_back(waveinfo[iLine].values[iWavelength]);
+    }
+
+  }
+
+  return iErr;
+
+}
+
+// --------------------------------------------------------------------------
+// Scale flux (intensity) at 1 AU to distance from the sun:
+// --------------------------------------------------------------------------
+
+int Euv::scale_from_1au(Planets planet,
+			Times time,
+			Inputs args) {
+
+  int iErr = 0;
+  float d = planet.get_star_to_planet_dist(time);
+  float scale = 1.0 / (d*d);
+
+  std::cout << "sun-planet-distance : " << d << "\n";
+
+  for (int iWave = 0; iWave < nWavelengths; iWave++)
+    wavelengths_intensity_top[iWave] = scale * wavelengths_intensity_1au[iWave];
+
+  return iErr;
+
+}
+
+// --------------------------------------------------------------------------
+// EUVAC
+// --------------------------------------------------------------------------
+
+int Euv::euvac(Times time,
+	       Indices indices,
+	       Inputs args) {
+
+  int iErr = 0;
+  float slope;
+
+  float f107 = indices.get_f107(time.get_current());
+  float f107a = indices.get_f107a(time.get_current());
+  float mean_f107 = (f107 + f107a)/2.0;
+
+  if (test_verbose(5,args.iVerbose))
+    std::cout << f107 << " " << f107a << "\n";
+
+  for (int iWave = 0; iWave < nWavelengths; iWave++) {
+
+    slope = 1.0 + euvac_afac[iWave] * (mean_f107 - 80.0);
+    if (slope < 0.8) slope = 0.8;
+    wavelengths_intensity_1au[iWave] = euvac_f74113[iWave] * slope * pcm2topm2;
+
+  }
+
+  if (test_verbose(5,args.iVerbose)) {
+
+    std::cout << "EUVAC output : "
+	      << f107 << " " << f107a
+	      << " -> " << mean_f107 << "\n";
+    for (int iWave = 0; iWave < nWavelengths; iWave++) {
+      std::cout << "     " << iWave << " "
+	   << wavelengths_short[iWave] << " "
+	   << wavelengths_long[iWave] << " "
+	   << wavelengths_intensity_1au[iWave] << "\n";
+    }
+
+  }
+
+  return iErr;
+
+}
