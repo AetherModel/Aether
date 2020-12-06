b1ab5e80f4d08c15013f01f09d187a63a8b123a5
Aaron Ridley <ridley@umich.edu>
2020-12-06 17:03:32 -0500

    FEAT: New reporting system for output

--- include/report.h
+++ include/report.h
@@ -5,8 +5,45 @@
 #define AETHER_INCLUDE_REPORT_H_
 
 #include <string>
+#include <vector>
 
-void report(int iLevel, std::string output_string, int iVerbose);
-int test_verbose(int iLevel, int iVerbose);
+class Report {
+
+public:
+
+  // Functions:
+
+  Report();
+  void set_verbose(int input);
+  void print(int iLevel, std::string output_string);
+  int test_verbose(int iLevel);
+  int get_verbose();
+  void enter(std::string);
+  void exit(std::string);
+  void times();
+  
+private:
+
+  int iVerbose;  
+  
+  struct item_struct {
+
+    std::string entry;
+    int nTimes;
+    float timing_total;
+    unsigned long long timing_start;
+    
+  };
+  
+  std::vector<item_struct> entries;
+  int nEntries;
+  std::string current_entry;
+
+  std::string divider;
+  int divider_length;
+  
+  int iLevel;
+
+};
 
 #endif // AETHER_INCLUDE_REPORT_H_
--- src/report.cpp
+++ src/report.cpp
@@ -1,8 +1,106 @@
 
 #include <string>
 #include <iostream>
+#include <chrono>
 
-void report(int iLevel, std::string output_string, int iVerbose) {
+#include "../include/report.h"
+
+
+// -----------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------
+
+Report::Report() {
+
+  current_entry = "";
+  nEntries = 0;
+  iVerbose = 0;
+  divider = ">";
+  divider_length = divider.length();
+  iLevel = 0;  
+  
+}
+
+// -----------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------
+
+void Report::enter(std::string input) {
+
+  current_entry = current_entry + divider + input;
+
+  int iEntry = -1;
+  for (int i=0; i < nEntries; i++) 
+    if (current_entry == entries[i].entry) iEntry = i;
+  if (iEntry == -1) {
+    item_struct tmp;
+    tmp.entry = current_entry;
+    tmp.nTimes = 0;
+    tmp.timing_total = 0.0;
+    entries.push_back(tmp);
+    nEntries++;
+    iEntry = nEntries-1;
+  }
+
+  // This was taken from
+  // https://stackoverflow.com/questions/19555121/how-to-get-current-timestamp-in-milliseconds-since-1970-just-the-way-java-gets
+  unsigned long long now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
+  entries[iEntry].timing_start = now;
+  iLevel++;
+
+  print(iLevel,"Entering function : "+current_entry);
+  
+}
+
+// -----------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------
+
+void Report::exit(std::string input) {
+
+  int iEntry = -1;
+  for (int i=0; i < nEntries; i++) 
+    if (current_entry == entries[i].entry) iEntry = i;
+  if (iEntry == -1) {
+    std::cout << "Report::exit Error!!! Could not find valid entry!\n";
+  } else {
+    unsigned long long now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    entries[iEntry].timing_total = entries[iEntry].timing_total +
+      float(now - entries[iEntry].timing_start)/1000.0;
+    entries[iEntry].nTimes++;
+    std::size_t pos = current_entry.find(input);
+    if (pos == std::string::npos) {
+      std::cout << "Report::exit Error!!! Could not find input : " << input << "!\n";
+      std::cout << "   current_entry : " << current_entry << "\n";
+    } else {
+      current_entry = current_entry.substr(0,pos-divider_length);
+      iLevel--;
+    }
+  }
+  
+}
+
+// -----------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------
+
+void Report::times() {
+
+  std::cout << "Timing Summary :\n";
+  for (int i=0; i < nEntries; i++) {
+    std::cout << entries[i].entry << "\n";
+    std::cout << "  nTimes called : " << entries[i].nTimes << "\n";
+    std::cout << "  timing_total (s) : " << entries[i].timing_total << "\n";
+  }
+
+}
+
+// -----------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------
+
+void Report::print(int iLevel, std::string output_string) {
 
   if (iLevel <= iVerbose) {
 
@@ -13,7 +111,11 @@ void report(int iLevel, std::string output_string, int iVerbose) {
 
 }
 
-int test_verbose(int iLevel, int iVerbose) {
+// -----------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------
+
+int Report::test_verbose(int iLevel) {
 
   int iPass = 0;
   if (iLevel <= iVerbose) {
@@ -24,4 +126,20 @@ int test_verbose(int iLevel, int iVerbose) {
 
   return iPass;
 
+}
+
+// -----------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------
+
+void Report::set_verbose(int input) {
+  iVerbose = input;
+}
+
+// -----------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------
+
+int Report::get_verbose() {
+  return iVerbose;
 }
