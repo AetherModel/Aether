f52a131c9eec115c1931b568f03778bcbe9c04e5
Aaron Ridley <ridley@umich.edu>
2020-12-06 16:27:58 -0500

    FEAT: move main loop details to advance.

---
+++ include/advance.h
@@ -0,0 +1,25 @@
+// (c) 2020, the Aether Development Team (see doc/dev_team.md for members)
+// Full license can be found in License.md
+
+#ifndef AETHER_INCLUDE_ADVANCE_H_
+#define AETHER_INCLUDE_ADVANCE_H_
+
+#include "../include/times.h"
+#include "../include/inputs.h"
+#include "../include/neutrals.h"
+#include "../include/euv.h"
+#include "../include/grid.h"
+#include "../include/planets.h"
+#include "../include/ions.h"
+
+int advance( Planets &planet,
+	     Grid &gGrid,
+	     Times &time,
+	     Euv &euv,
+	     Neutrals &neutrals,
+	     Ions &ions,
+	     Indices &indices,
+	     Inputs &args,
+	     Report &report);
+
+#endif // AETHER_INCLUDE_ADVANCE_H_
---
+++ src/add_sources.cpp
@@ -0,0 +1,43 @@
+// (c) 2020, the Aether Development Team (see doc/dev_team.md for members)
+// Full license can be found in License.md
+
+#include <iostream>
+#include <string>
+
+#include "../include/neutrals.h"
+#include "../include/times.h"
+#include "../include/inputs.h"
+#include "../include/report.h"
+
+void Neutrals::add_sources( Times time, Report &report) {
+
+  std::string function="add_sources";
+  report.enter(function);  
+
+  long iLon, iLat, iAlt, index;
+
+  float dt = time.get_dt();
+  
+  for (iLon = 0; iLon < nGeoLonsG; iLon++) {
+    for (iLat = 0; iLat < nGeoLatsG; iLat++) {
+      for (iAlt = 0; iAlt < nGeoAltsG; iAlt++) {
+	
+	index = ijk_geo_s3gc(iLon,iLat,iAlt);
+
+	temperature_s3gc[index] =
+	  temperature_s3gc[index] +
+	  dt * ( heating_euv_s3gc[index] +
+		 conduction_s3gc[index]);
+
+	if (iAlt == 40 && iLon == nGeoLonsG/2 && iLat == nGeoLonsG/2 )
+	  std::cout << "sources(t) : " << heating_euv_s3gc[index]
+	       << " " << conduction_s3gc[index] << "\n";
+
+      }
+    }
+  }
+  
+  report.exit(function);  
+  return;
+  
+}
