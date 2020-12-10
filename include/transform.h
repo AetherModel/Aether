f7cc12cd18848e3689aaedc5ff5eb63d0b21bbc5
Aaron Ridley <ridley@umich.edu>
2020-12-09 22:50:12 -0500

    FEAT: rotations and differences

--- include/transform.h
+++ include/transform.h
@@ -4,7 +4,28 @@
 #ifndef AETHER_INCLUDE_TRANSFORM_H_
 #define AETHER_INCLUDE_TRANSFORM_H_
 
-void transform_llr_to_xyz(float llr[3], float xyz[3]);
+void transform_llr_to_xyz(float llr_in[3], float xyz_out[3]);
+void transform_rot_z(float xyz_in[3], float angle_in, float xyz_out[3]);
+void transform_rot_y(float xyz_in[3], float angle_in, float xyz_out[3]);
+void transform_float_vector_to_array(std::vector<float> input,
+				     float output[3]);
+
+void transform_vector_xyz_to_env(float xyz_in[3],
+				 float lon,
+				 float lat,
+				 float env_out[3]);
+
+// These are not really transformations, but are 
+
+void vector_diff(float vect_in_1[3],
+		 float vect_in_2[3],
+		 float vect_out[3]);
+
+void get_vector_component(float *vector_in_v3gc,
+			  int iComponent,
+			  int IsGeoGrid,
+			  float *component_out_s3gc);
+
 
 #endif // AETHER_INCLUDE_TRANSFORM_H_
 
--- src/transform.cpp
+++ src/transform.cpp
@@ -2,21 +2,154 @@
 // Full license can be found in License.md
 
 #include <math.h>
+#include <vector>
+
+#include "../include/sizes.h"
+#include "../include/grid.h"
 
 // -----------------------------------------------------------------------
 // Transform Longitude, Latitude, Radius to X, Y, Z
 // -----------------------------------------------------------------------
 
-void transform_llr_to_xyz(float llr[3], float xyz[3]) {
+void transform_llr_to_xyz(float llr_in[3], float xyz_out[3]) {
 
-  // llr[0] = longitude (in radians)
-  // llr[1] = latitude (in radians)
-  // llr[2] = radius
+  // llr_in[0] = longitude (in radians)
+  // llr_in[1] = latitude (in radians)
+  // llr_in[2] = radius
   
-  xyz[0] = llr[2] * cos(llr[1]) * cos(llr[0]);
-  xyz[1] = llr[2] * cos(llr[1]) * sin(llr[0]);
-  xyz[2] = llr[2] * sin(llr[1]);
+  xyz_out[0] = llr_in[2] * cos(llr_in[1]) * cos(llr_in[0]);
+  xyz_out[1] = llr_in[2] * cos(llr_in[1]) * sin(llr_in[0]);
+  xyz_out[2] = llr_in[2] * sin(llr_in[1]);
   
 }
 
+// -----------------------------------------------------------------------
+// Rotate around the z-axis
+//  - Angle needs to be in radians!!!
+// -----------------------------------------------------------------------
 
+void transform_rot_z(float xyz_in[3], float angle_in, float xyz_out[3]) {
+
+  float ca = cos(angle_in);
+  float sa = sin(angle_in);
+
+  xyz_out[0] =  xyz_in[0] * ca + xyz_in[1] * sa;
+  xyz_out[1] = -xyz_in[0] * sa + xyz_in[1] * ca;
+  xyz_out[2] =  xyz_in[2];
+
+  return;
+  
+}
+
+// -----------------------------------------------------------------------
+// Rotate around the y-axis
+//  - Angle needs to be in radians!!!
+// -----------------------------------------------------------------------
+
+void transform_rot_y(float xyz_in[3], float angle_in, float xyz_out[3]) {
+
+  float ca = cos(angle_in);
+  float sa = sin(angle_in);
+
+  xyz_out[0] = xyz_in[0] * ca - xyz_in[2] * sa;
+  xyz_out[1] = xyz_in[1];
+  xyz_out[2] = xyz_in[0] * sa + xyz_in[2] * ca;
+
+  return;
+  
+}
+
+// -----------------------------------------------------------------------
+// Simply move data from a vector to a C-native array (float)
+// -----------------------------------------------------------------------
+
+void transform_float_vector_to_array(std::vector<float> input,
+				     float output[3]) {
+
+  for (int i=0; i<3; i++) output[i] = input[i];
+
+  return;
+}
+
+// -----------------------------------------------------------------------
+// Rotate a vector from XYZ to East, North, Vertical
+// -----------------------------------------------------------------------
+
+void transform_vector_xyz_to_env(float xyz_in[3],
+				 float lon,
+				 float lat,
+				 float env_out[3]) {
+
+  env_out[2] =   xyz_in[0] * cos(lat)*cos(lon) + xyz_in[1] * cos(lat)*sin(lon) + xyz_in[2]*sin(lat);
+  env_out[1] = -(xyz_in[0] * sin(lat)*cos(lon) + xyz_in[1] * sin(lat)*sin(lon) - xyz_in[2]*cos(lat));
+  env_out[0] = - xyz_in[0] * sin(lon)          + xyz_in[1] * cos(lon);
+  
+  return;
+
+}
+
+// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
+// These are not realy transform functions.  I need a better place to
+// put them.
+// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
+
+// -----------------------------------------------------------------------
+// Simple 3-element vector difference
+// -----------------------------------------------------------------------
+
+void vector_diff(float vect_in_1[3],
+		 float vect_in_2[3],
+		 float vect_out[3]) {
+  
+  for (int i=0; i<3; i++) vect_out[i] = vect_in_1[i] - vect_in_2[i];
+
+  return;
+
+}
+
+// -----------------------------------------------------------------------
+// grab one component of a vector
+// -----------------------------------------------------------------------
+
+void get_vector_component(float *vector_in_v3gc,
+			  int iComponent,
+			  int IsGeoGrid,
+			  float *component_out_s3gc) {
+
+  long nLons, nLats, nAlts, iLon, iLat, iAlt, index, indexv;
+
+  if (IsGeoGrid) {
+    nLons = nGeoLonsG;
+    nLats = nGeoLatsG;
+    nAlts = nGeoAltsG;
+  } else {
+    nLons = nMagLonsG;
+    nLats = nMagLatsG;
+    nAlts = nMagAltsG;
+  }
+
+  for (iLon = 0; iLon < nLons; iLon++) {
+    for (iLat = 0; iLat < nLats; iLat++) {
+      for (iAlt = 0; iAlt < nAlts; iAlt++) {
+
+	if (IsGeoGrid) {
+	  index = ijk_geo_s3gc(iLon,iLat,iAlt);
+	  indexv = ijkl_geo_v3gc(iLon,iLat,iAlt,iComponent);
+	} else {
+	  index = ijk_mag_s3gc(iLon,iLat,iAlt);
+	  indexv = ijkl_mag_v3gc(iLon,iLat,iAlt,iComponent);
+	}
+
+	component_out_s3gc[index] = vector_in_v3gc[indexv];
+
+      }
+    }
+  }
+
+  return;
+  
+}
