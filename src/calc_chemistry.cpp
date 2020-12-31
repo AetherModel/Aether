594a94a9612ca9b4fb2364ad907a1da58e25f9bf
Aaron Ridley <ridley@umich.edu>
2020-12-31 11:42:07 -0500

    DOC: Add some comments and clean code a bit.

--- src/calc_chemistry.cpp
+++ src/calc_chemistry.cpp
@@ -19,35 +19,23 @@ void Chemistry::calc_chemistry(Neutrals &neutrals,
 			       Grid grid,
 			       Report &report) {
 
-  long nLons, nLats, nAlts, iLon, iLat, iAlt, index;
   int iSpecies;
 
   std::string function = "Chemistry::calc_chemistry";
   static int iFunction = -1;
   report.enter(function, iFunction);  
 
-  nLons = grid.get_nLons();
-  nLats = grid.get_nLats();
-  nAlts = grid.get_nAlts();
-
-  float neutral_density[nSpecies];
-  float ion_density[nIons+1];  // add one for electron density
-  float neutral_sources[nSpecies];
-  float neutral_losses[nSpecies];
-  float ion_sources[nIons];
-  float ion_losses[nIons];
-
-  float Tn, Te, Ti;
-
   float dt = time.get_dt();
-  float old_density, source, loss, new_density;
-  float heat_neutrals, heat_ions, heat_electrons;
   
   // ------------------------------------
   // Calculate electron densities
   // ------------------------------------
 
   ions.fill_electrons(report);
+
+  // ----------------------------------------------------------
+  // Initialize the sources and losses with EUV stuff:
+  // ----------------------------------------------------------
 
   for (iSpecies=0; iSpecies < nSpecies; iSpecies++) {
     neutrals.neutrals[iSpecies].losses_scgc = neutrals.neutrals[iSpecies].ionization_scgc;
@@ -59,9 +47,15 @@ void Chemistry::calc_chemistry(Neutrals &neutrals,
     ions.species[iSpecies].sources_scgc = ions.species[iSpecies].ionization_scgc;
   }
 
+  // ----------------------------------------------------
+  // Calculate the chemical sources and losses
+  // ----------------------------------------------------
+
   calc_chemical_sources(neutrals, ions, report);
 
-  fcube norm_loss = neutrals.neutrals[0].losses_scgc;
+  // ---------------------------------------------------------
+  // Once sources and losses are done, solve for new densities
+  // ---------------------------------------------------------
 
   for (iSpecies=0; iSpecies < nSpecies; iSpecies++) {
     neutrals.neutrals[iSpecies].density_scgc =
@@ -78,6 +72,10 @@ void Chemistry::calc_chemistry(Neutrals &neutrals,
 			   ions.species[iSpecies].losses_scgc,
 			   dt);
   }
+
+  // ---------------------------------------------------------
+  // Recalculate electrons
+  // ---------------------------------------------------------
 
   ions.fill_electrons(report);
 
