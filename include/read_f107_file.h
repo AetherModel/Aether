983f0bfa80a98c29ec008c4f2b60fb2ab35235ed
Aaron Ridley <ridley@umich.edu>
2021-01-24 19:52:23 -0500

    FEAT: Change output from reading files.

--- include/read_f107_file.h
+++ include/read_f107_file.h
@@ -1,14 +1,25 @@
 // Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
 // Full license can be found in License.md
 
-#ifndef INCLUDE_READ_F107_FILE_H_
-#define INCLUDE_READ_F107_FILE_H_
+#ifndef INCLUDE_READ_INDICES_H_
+#define INCLUDE_READ_INDICES_H_
 
 #include <vector>
 #include <string>
 
-int read_f107_file(std::string f107_file,
-                   std::vector<double> &time,
-                   std::vector<float> &f107);
+#include "indices.h"
+#include "report.h"
 
-#endif  // INCLUDE_READ_F107_FILE_H_
+index_file_output_struct read_f107_file(std::string f107_file,
+					Indices indices,
+					Report &report);
+index_file_output_struct read_omni_file(std::string omni_file,
+					Indices indices,
+					Report &report);
+int pair_omniweb_index(std::string, Indices index);
+float get_omniweb_missing_value(std::string);
+
+int read_and_store_indices(Indices &indices, Inputs args, Report &report);
+
+
+#endif  // INCLUDE_READ_INDICES_H_
