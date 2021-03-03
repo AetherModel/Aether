c4a52c7e381e20b68e50fd5405c001f7e0649dab
Aaron James Ridley <ridley@umich.edu>
2021-03-03 16:20:14 -0500

    FEAT: irrelavent stuff now. Grid more flexible.

--- include/sizes.h
+++ include/sizes.h
@@ -12,20 +12,24 @@
 
 #define nGeoGhosts 2
 
-#define nGeoAlts 60
-#define nGeoAltsG nGeoGhosts + nGeoAlts + nGeoGhosts
-#define iGeoAltStart_ nGeoGhosts // Inclusive!!!
-#define iGeoAltEnd_ nGeoGhosts + nGeoAlts - 1 // Inclusive!!!
+// int nGeoAlts = 60;
+// int nGeoAltsG = nGeoGhosts + nGeoAlts + nGeoGhosts;
+// int iGeoAltStart_ = nGeoGhosts; // Inclusive!!!
+// int iGeoAltEnd_ = nGeoGhosts + nGeoAlts - 1; // Inclusive!!!
+//#define nGeoAlts 60
+//#define nGeoAltsG nGeoGhosts + nGeoAlts + nGeoGhosts
+//#define iGeoAltStart_ nGeoGhosts // Inclusive!!!
+//#define iGeoAltEnd_ nGeoGhosts + nGeoAlts - 1 // Inclusive!!!
 
-#define nGeoLons 18
-#define nGeoLonsG nGeoGhosts + nGeoLons + nGeoGhosts
-#define iGeoLonStart_ nGeoGhosts // Inclusive!!!
-#define iGeoLonEnd_ nGeoGhosts + nGeoLons - 1 // Inclusive!!!
-
-#define nGeoLats 36
-#define nGeoLatsG nGeoGhosts + nGeoLats + nGeoGhosts
-#define iGeoLatStart_ nGeoGhosts // Inclusive!!!
-#define iGeoLatEnd_ nGeoGhosts + nGeoLats - 1 // Inclusive!!!
+//#define nGeoLons 18
+//#define nGeoLonsG nGeoGhosts + nGeoLons + nGeoGhosts
+//#define iGeoLonStart_ nGeoGhosts // Inclusive!!!
+//#define iGeoLonEnd_ nGeoGhosts + nGeoLons - 1 // Inclusive!!!
+//
+//#define nGeoLats 36
+//#define nGeoLatsG nGeoGhosts + nGeoLats + nGeoGhosts
+//#define iGeoLatStart_ nGeoGhosts // Inclusive!!!
+//#define iGeoLatEnd_ nGeoGhosts + nGeoLats - 1 // Inclusive!!!
 
 // This is for the magnetic grid:
 
