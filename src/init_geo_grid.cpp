f07f60aa9336c9701fb39a9a604d6cfde09050ef
Aaron James Ridley <ridley@umich.edu>
2021-03-03 16:33:51 -0500

    FEAT: aether.h added. BUG: grid actually calculated correctly now.

--- src/init_geo_grid.cpp
+++ src/init_geo_grid.cpp
@@ -2,16 +2,8 @@
 // Full license can be found in License.md
 
 #include <iostream>
-#include <armadillo>
 
-#include "../include/inputs.h"
-#include "../include/report.h"
-#include "../include/grid.h"
-#include "../include/planets.h"
-#include "../include/sizes.h"
-#include "../include/fill_grid.h"
-
-using namespace arma;
+#include "aether.h"
 
 void Grid::init_geo_grid(Planets planet, Inputs input, Report &report) {
 
@@ -30,9 +22,10 @@ void Grid::init_geo_grid(Planets planet, Inputs input, Report &report) {
   // - Make a 1d vector
   // - copy it into the 3d cube
   fvec lon1d(nLons);
-  float dlon = (grid_input.lon_max - grid_input.lon_min) / nGeoLons;
+  float dlon = (grid_input.lon_max - grid_input.lon_min) / (nLons-2*nGCs);
+  std::cout << "dlon : " << dlon/dtor << "\n";
   for (iLon=0; iLon < nLons; iLon++)
-    lon1d(iLon) = (iLon-nGCs+0.5) * dlon;
+    lon1d(iLon) = grid_input.lon_min + (iLon-nGCs+0.5) * dlon; 
 
   for (iLat=0; iLat < nLats; iLat++) {
     for (iAlt=0; iAlt < nAlts; iAlt++) {
@@ -44,7 +37,8 @@ void Grid::init_geo_grid(Planets planet, Inputs input, Report &report) {
   // - Make a 1d vector
   // - copy it into the 3d cube
   fvec lat1d(nLats);
-  float dlat = (grid_input.lat_max - grid_input.lat_min) / nGeoLats;
+  float dlat = (grid_input.lat_max - grid_input.lat_min) / (nLats-2*nGCs);
+  std::cout << "dlat : " << dlat/dtor << "\n";
   for (iLat=0; iLat < nLats; iLat++)
     lat1d(iLat) = grid_input.lat_min + (iLat-nGCs+0.5) * dlat;
 
