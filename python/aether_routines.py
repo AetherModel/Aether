0b632cca0193f54f9c937074d92ecccdb591cf92
Aaron Ridley <ridley@umich.edu>
2021-03-10 15:46:57 -0500

    STY: Improve coding style.

--- python/aether_routines.py
+++ python/aether_routines.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
 
-from datetime import datetime
-from datetime import timedelta
-import numpy as np
 from netCDF4 import Dataset
-
-#-----------------------------------------------------------------------------
-# Get some ancillary information from the netcdf file.  This simply
-# mirrors the gitm header info.
-# Inputs:
-#  - filelist: a list of netcdf files. Really, only the last file is used.
-#              also, the number of files is recorded.
-# Output:
-#  - header: A dictionary containing information about the netcdf file, such
-#            as nLons, nLons, nAlts, nVars, variable names, time(s)
-#-----------------------------------------------------------------------------
+import datetime as dt
+import numpy as np
 
 def read_aether_header(filelist):
+    r""" Grab ancillary information from the netcdf file
 
-    header = {}
-    header["nFiles"] = len(filelist)
-    header["version"] = 0.1
-    header["nLons"] = 0
-    header["nLats"] = 0
-    header["nAlts"] = 0
-    header["nVars"] = 0
-    header["vars"] = []
-    header["time"] = []
-    header["filename"] = []
+    Get some ancillary information from the netcdf file.  This simply
+    mirrors the gitm header info.
 
-    header["filename"].append(filelist[-1])
+    Parameters
+    ----------
+    filelist: a list of netcdf files. Really, only the last file is used.
+              also, the number of files is recorded.
+
+    Returns
+    -------
+    header: A dictionary containing information about the netcdf file, such
+            as nLons, nLons, nAlts, nVars, variable names, time(s)
+    """
+
+    header = {"nFiles": len(filelist), \
+              "version": 0.1, \
+              "nLons": 0, \
+              "nLats": 0, \
+              "nAlts": 0, \
+              "nVars": 0, \
+              "vars": [], \
+              "time": [], \
+              "filename": [filelist[-1]] }
     
-    ncfile = Dataset(filelist[-1],'r')
+    ncfile = Dataset(filelist[-1], 'r')
     header["nVars"] = 0
     for var in ncfile.variables.values():
         s = var.shape
@@ -40,11 +40,11 @@ def read_aether_header(filelist):
             header["nLats"] = s[1]
             header["nAlts"] = s[2]
             header["vars"].append(var.name)
-            header["nVars"] = header["nVars"] + 1
+            header["nVars"] += 1
 
-    base = datetime(1965,1,1,0,0,0)
+    base = dt.datetime(1965, 1, 1, 0, 0, 0)
     time = np.array(ncfile.variables['Time'])
-    current = base + timedelta(seconds = time[0])
+    current = base + dt.timedelta(seconds = time[0])
     header["time"].append(current)
             
     ncfile.close()
@@ -62,20 +62,32 @@ def read_aether_header(filelist):
 #-----------------------------------------------------------------------------
 
 def read_aether_one_file(file, vars):
+    r""" Read in list of variables from a single netCDF file
+
+    Parameters
+    ----------
+    file: netcdf file to read
+    vars: list of variable NAMES to read
+
+    Returns
+    -------
+    data["time"]: datetime of the file
+    data[NUMBER]: data that is read in.
+                  NUMBER goes from 0 - number of vars read in (0-3 typical)
+
+    """
 
     data = {}
     
-    ncfile = Dataset(file,'r')
+    ncfile = Dataset(file, 'r')
 
-    base = datetime(1965,1,1,0,0,0)
+    base = dt.datetime(1965, 1, 1, 0, 0, 0)
     time = np.array(ncfile.variables['Time'])
-    current = base + timedelta(seconds = time[0])
+    current = base + dt.timedelta(seconds = time[0])
     data["time"] = current
 
-    iVar = 0
-    for var in vars:
+    for iVar, var in enumerate(vars):
         data[iVar] = np.array(ncfile.variables[var])
-        iVar = iVar + 1
     ncfile.close()
 
     return data
