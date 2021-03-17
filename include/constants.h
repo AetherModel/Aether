0defc959a44c920dbbc4db0462b8fa444213f382
Aaron Ridley <ridley@umich.edu>
2021-03-17 11:27:17 -0400

    STY: Enforce naming conventions for constants

--- include/constants.h
+++ include/constants.h
@@ -6,50 +6,124 @@
 
 #include <vector>
 
-// Header file to define a bunch of physical constants
+// -------------------------------------------------------------------------
+// Header file to define a bunch of constants
+// -------------------------------------------------------------------------
 
-const float avogadros_number = 6.02214086e23;  // per mole
-const float boltzmanns_constant = 1.38064852e-23;  // m2 kg /s2 /K
-const float mass_proton = 1.6726219e-27;  // kg
-const float amu = mass_proton;
-const float mass_electron = 9.1094e-31;  // Kg
-const float planck_constant = 6.6261e-34;  // Js
-const float element_charge = 1.6022e-19;  // C (J/eV)
-const float speed_light = 2.9979e8;  // m/s
-const float univ_gas_constant = avogadros_number*boltzmanns_constant;
-const float r_gas = univ_gas_constant*1.0E+07;  // erg/(moleK)
-const float pi = 3.141592653589793;
-const float twopi = 2*pi;
-const float dtor = pi/180.0;
-const float rtod = 180.0/pi;
-const float p00 = 1.0e5;  // Pa
-const float T00 = 273.0;  // K
-const float SpecificHeatVolume = 3./2.;
-const float SpecificHeatPressure = SpecificHeatVolume + 1.0;
-const float gamma_const = SpecificHeatPressure/SpecificHeatVolume;
 
-// A bunch of constants for converting time between seconds and arrays:
-const std::vector<int> days_of_month {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-const int reference_year = 1965;
-const double julian_day_of_reference = 2438762.0;
-const double j2000 = 2451545.0;
-const double seconds_per_year = 31536000.0;
-const double seconds_per_day = 86400.0;
-const double seconds_per_hour = 3600.0;
-const double seconds_per_minute = 60.0;
+// -------------------------------------------------------------------------
+// Physical Constants
+//   - Naming standards:
+//     - Names start with a "c" to indicate they are constants
+//     - Names should be all CAPS (except for "c" at start)
+//     - Should use common abbreviations for names and not whole names
+// -------------------------------------------------------------------------
 
-// Conversion
-const float mtokm = 1.0/1000.0;
-const float kmtom = 1000.0;
+// avogadros_number (per mole)
+const float cNA = 6.02214086e23;
+
+// boltzmanns_constant (m2 kg /s2 /K)
+const float cKB = 1.38064852e-23;
+
+// Universal Gas Constant (J/K/mol)
+const float cR = cNA * cKB;
+
+// Mass of a proton (kg)
+const float cMP = 1.6726219e-27;
+
+// Atomic Mass Unit (kk)
+const float cAMU = cMP;
+
+// Mass of an electron (kg)
+const float cME = 9.1094e-31;
+
+// Plancks Constant (Js)
+const float cH = 6.6261e-34;
+
+// Elemental Charge (C (J/eV))
+const float cE = 1.6022e-19;
+
+// Speed of Light (m/s)
+const float cC = 2.9979e8;
+
+// -------------------------------------------------------------------------
+// Stellar constants:
+// -------------------------------------------------------------------------
+
+// gravitational_constant (m3/kg/s2)
+const float cG = 6.67408e-11;
+
+// This is for our sun (solar mass in kg):
+const float cMSOL = 1.989e30;
+
+// -------------------------------------------------------------------------
+// Some constants for time
+// -------------------------------------------------------------------------
+
+const std::vector<int> cDAYS {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int cREFYEAR = 1965;
+const double cREFJULIAN = 2438762.0;
+const double cJULIAN2000 = 2451545.0;
+
+// -------------------------------------------------------------------------
+// Some Constants for Angles
+// -------------------------------------------------------------------------
+
+const float cPI = 3.141592653589793;
+const float cTWOPI = 2*cPI;
+
+// -------------------------------------------------------------------------
+// Conversion Constants:
+// - Naming standards:
+//   - Names start with a "c" to indicate they are constants
+//   - Names have a lower-case "to" in them to indicate conversion
+//   - Names are all UPPER CASE otherwise
+// -------------------------------------------------------------------------
+
+const float cDtoR = cPI/180.0;
+const float cRtoD = 180.0/cPI;
+
+// -------------------------------------------------------------------------
+// converting time between seconds and other units of time:
+// -------------------------------------------------------------------------
+
+// Years <-> Seconds:
+const double cYtoS = 31536000.0;
+const double cStoY = 1.0 / cYtoS;
+
+// Days <-> Seconds:
+const double cDtoS = 86400.0;
+const double cStoD = 1.0 / cDtoS;
+
+// Hours <-> Seconds:
+const double cHtoS = 3600.0;
+const double cStoH = 1.0 / cHtoS;
+
+// Minutes <-> Seconds:
+const double cMtoS = 60.0;
+const double cStoM = 1.0 / cMtoS;
+
+// MilliSeconds <-> Seconds:
+const double cMStoS = 1.0/1000.0;
+const double cStoMS = 1000.0;
+
+// -------------------------------------------------------------------------
+// converting lengths between meters and other units of measurement:
+// -------------------------------------------------------------------------
+
+// kilometer <-> meter:
+const float cKMtoM = 1000.0;
+const float cMtoKM = 1.0 / cKMtoM;
+
+// Angstrom <-> Meter
+const float cAtoM = 1.0e-10;
+
+// AU <-> Meter
+const float cAUtoM = 1.495978707e11;
+
 const float pcm3topm3 = 1e6;  // /cm3 to /m3
 const float pcm2topm2 = 1e4;  // /cm2 to /m2
 const float pcmtopm = 100.0;  // /cm to /m
-const float atom = 1.0e-10;  // angstrom to m
 
-// Stellar constants:
-const float au_to_m = 1.495978707e11;  // m
-const float gravitational_constant = 6.67408e-11;  // m3/kg/s2
-// This is for our sun:
-const float solar_mass = 1.989e30;  // kg
 
 #endif  // INCLUDE_CONSTANTS_H_
--- include/indices.h
+++ include/indices.h
@@ -33,14 +33,14 @@ class Indices {
 
   int get_f107_index_id();
   int get_f107a_index_id();
-  int get_imf_bx_index_id();
-  int get_imf_by_index_id();
-  int get_imf_bz_index_id();
-  int get_sw_vx_index_id();
-  int get_sw_vy_index_id();
-  int get_sw_vz_index_id();
-  int get_sw_n_index_id();
-  int get_sw_t_index_id();
+  int get_imfbx_index_id();
+  int get_imfby_index_id();
+  int get_imfbz_index_id();
+  int get_swvx_index_id();
+  int get_swvy_index_id();
+  int get_swvz_index_id();
+  int get_swn_index_id();
+  int get_swt_index_id();
   int get_ae_index_id();
   int get_au_index_id();
   int get_al_index_id();
@@ -65,19 +65,19 @@ class Indices {
 
   std::vector<index_time_pair> all_indices_arrays;
 
-  int f107_ = 0;
-  int f107a_ = 1;
-  int imf_bx_ = 2;
-  int imf_by_ = 3;
-  int imf_bz_ = 4;
-  int sw_vx_ = 5;
-  int sw_vy_ = 6;
-  int sw_vz_ = 7;
-  int sw_n_ = 8;
-  int sw_t_ = 9;
-  int ae_ = 10;
-  int al_ = 11;
-  int au_ = 12;
+  const int iF107_ = 0;
+  const int iF107A_ = 1;
+  const int iIMFBX_ = 2;
+  const int iIMFBY_ = 3;
+  const int iIMFBZ_ = 4;
+  const int iSWVX_ = 5;
+  const int iSWVY_ = 6;
+  const int iSWVZ_ = 7;
+  const int iSWN_ = 8;
+  const int iSWT_ = 9;
+  const int iAE_ = 10;
+  const int iAL_ = 11;
+  const int iAU_ = 12;
   int nIndices = 13;
   
   float get_index(double time, int index);
