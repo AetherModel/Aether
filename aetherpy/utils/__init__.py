3ef2db65f7a1b28e67a6e9e475b83b908bc6820d
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-03-25 13:57:18 -0400

    FEAT: adds inputs import
    Added an import command for the new inputs module.

--- aetherpy/utils/__init__.py
+++ aetherpy/utils/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
 # Full license can be found in License.md
 
-from aetherpy.utils import time_conversion
+from aetherpy.utils import inputs, time_conversion
