491183a8de4c5026fa84ec0afeb54e62e6e414ea
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-03-25 13:55:59 -0400

    DOC: updated copyright
    Updated the copyright and license line in the time_conversion file.

--- aetherpy/utils/time_conversion.py
+++ aetherpy/utils/time_conversion.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
+# Full license can be found in License.md
 """Routines to perform temporal calculations
 """
 
