94dac3a5fc466bd53b325c726fac6a61a4e5e081
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-03-25 12:33:34 -0400

    MAINT: moved time utilities
    Created a utils sub-module and moved the time routines into this module.

---
+++ aetherpy/utils/time_conversion.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""Routines to perform temporal calculations
+"""
+
+import datetime as dt
+
+
+def epoch_to_datetime(epoch_time):
+    """Convert from epoch seconds to datetime
+
+    Parameters
+    ----------
+    epoch_time : int
+        Seconds since 1 Jan 1965
+
+    Returns
+    -------
+    dtime : dt.datetime
+        Datetime object corresponding to `epoch_time`
+
+    """
+
+    dtime = dt.datetime(1965, 1, 1) + dt.timedelta(seconds=epoch_time)
+
+    return dtime
+
+
+def datetime_to_epoch(dtime):
+    """Convert datetime to epoch seconds
+
+    Parameters
+    ----------
+    dtime : dt.datetime or dt.date
+        Datetime object
+
+    Returns
+    -------
+    epoch_time : float
+        Seconds since 1 Jan 1965
+
+    """
+
+    epoch_time = (dtime - dt.datetime(1965, 1, 1)).total_seconds()
+
+    return epoch_time
