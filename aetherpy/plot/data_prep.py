3900e91892c9a3abb84e75c705dc1acc95c3ba5f
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-03-26 09:54:29 -0400

    FEAT: add TEC calculation routine
    Added a routine to calculate the TEC over a specified altitude range.  Also updated the `get_cut_index` routine to return a tuple for slicing data of the expected dimensions.

--- aetherpy/plot/data_prep.py
+++ aetherpy/plot/data_prep.py
@@ -31,6 +31,8 @@ def get_cut_index(lons, lats, alts, cut_val, cut_coord='alt'):
     -------
     icut : int
         Cut index
+    cut_data : tuple
+        Tuple to select data of the expected dimensions along `icut`
     x_coord : array-like
         Array of data to include along the x-axis
     y_coord : array-like
@@ -74,6 +76,8 @@ def get_cut_index(lons, lats, alts, cut_val, cut_coord='alt'):
                 icut = alts.shape[0] - 3
 
         z_val = alts[icut]
+        cut_data = (slice(0, lons.shape[0], 1), slice(0, lats.shape[0], 1),
+                    icut)
     if cut_coord == 'lat':
         # Find closest value for latitude in between sensible limits
         if cut_val < lats[1] or cut_val > lat[-2]:
@@ -84,6 +88,8 @@ def get_cut_index(lons, lats, alts, cut_val, cut_coord='alt'):
             icut = abs(lats - cut_val).argmin()
 
         z_val = lats[icut]
+        cut_data = (slice(0, lons.shape[0], 1), icut,
+                    slice(0, alts.shape[0], 1))
     if cut_coord == 'lon':
         # Find closest value for longitude in between sensible limits
         if cut_val < lons[1] or cut_val > lons[-2]:
@@ -94,6 +100,57 @@ def get_cut_index(lons, lats, alts, cut_val, cut_coord='alt'):
             icut = abs(lons - cut_val).argmin()
 
         z_val = lons[icut]
+        cut_data = (icut, slice(0, lats.shape[0], 1),
+                    slice(0, alts.shape[0], 1))
 
-    return icut, x_coord, y_coord, z_val
+    return icut, cut_data, x_coord, y_coord, z_val
+
+
+def calc_tec(alt, ne, ialt_min=2, ialt_max=-4):
+    """Calculate the TEC for the specified altitude range from electron density
+
+    Parameters
+    ----------
+    alt : array-like
+        1D Altitude data in km
+    ne : array-like
+        Electron density in cubic meters, with altitude as the last axis
+    ialt_min : int
+        Lowest altitude index to use (default=2)
+    ialt_max : int
+        Highest altitude index to use, may be negative (default=-4)
     
+    Returns
+    -------
+    tec : array-like
+        Array of TEC values in TECU
+
+    Notes
+    -----
+    TEC = Total Electron Content
+    TECU = 10^16 m^-2
+
+    """
+    alt = np.asarray(alt)
+    ne = np.asarray(ne)
+
+    # Initialize the TEC
+    tec = np.zeros(shape=ne[..., 0].shape())
+
+
+    # Get the range of altitudes to cycle over
+    if ialt_max < 0:
+        ialt_max += alt.shape[0]
+
+    if ialt_max <= ialt_min:
+        raise ValueError('`ialt_max` must be greater than `ialt_min`')
+
+    # Cycle through each altitude bin, summing the contribution from the
+    # electron density
+    for ialt in np.arange(ialt_min, ialt_max, 1):
+        tec += 1000.0 * ne[..., ialt] * (alts[ialt + 1] - alts[ialt - 1]) / 2.0
+
+    # Convert TEC from per squared meters to TECU
+    tec /= 1.0e16
+
+    return tec
