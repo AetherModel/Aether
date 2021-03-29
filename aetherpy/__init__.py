2aacff987af907054eec5b1b75db1e318860071e
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-03-29 09:22:34 -0400

    BUG: moves logger definition
    Moved the logger definition to occur before the sub-module imports, so that it is always defined.

--- aetherpy/__init__.py
+++ aetherpy/__init__.py
@@ -2,13 +2,14 @@
 # Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
 # Full license can be found in License.md
 
+# Define a logger object to allow easier log handling
 import logging
 
+logging.raiseExceptions = False
+logger = logging.getLogger('aetherpy_logger')
+
+# Import the sub-modules
 from aetherpy import io, utils, plot
 
 # Define global variables
 __version__ = '0.0.1a'
-
-# Define a logger object to allow easier log handling
-logging.raiseExceptions = False
-logger = logging.getLogger('aetherpy_logger')
