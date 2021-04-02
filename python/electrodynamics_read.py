cfa83464377606db08e575085d8bd326d448ebb9
Aaron Ridley <ridley@umich.edu>
2021-04-02 16:27:50 -0400

    FEAT: Make compatable with AMIE output format.

--- python/electrodynamics_read.py
+++ python/electrodynamics_read.py
@@ -7,13 +7,12 @@ import matplotlib.pyplot as plt
 from pylab import cm
 from datetime import datetime
 from datetime import timedelta
+import sys
 
-file = 'test.nc'
+args = sys.argv
+
+file = args[1]
 ncfile = nc.Dataset(file, 'r')
-
-for var in ncfile.variables.values():
-    print(var.name)
-    print(var.shape)
 
 lats = np.array(ncfile.variables['MagneticLatitude'])
 mlts = np.array(ncfile.variables['MagneticLocalTime'])
@@ -21,30 +20,23 @@ potential = np.array(ncfile.variables['Potential'])
 eflux = np.array(ncfile.variables['EFlux'])
 times_in_s = np.array(ncfile.variables['Time'])
 
-print(times_in_s)
 nTimes = len(times_in_s)
 base = datetime(1965,1,1,0,0,0)
 times = []
 for t in times_in_s:
     times.append(base + timedelta(seconds = t))
 
-print(lats)
-print(mlts)
-r, theta = np.meshgrid(90.0 - lats, mlts * np.pi/12.0 - np.pi/2.0)
-
-#maxi = np.max(np.abs(potential))
-#mini = -maxi
+theta, r = np.meshgrid(mlts * np.pi/12.0 - np.pi/2.0, 90.0 - lats)
 
 maxi = np.max(np.abs(eflux))
 mini = 0.0
 
-fig = plt.figure(figsize = (6,6))
+fig = plt.figure(figsize = (10,10))
 ax = fig.add_subplot(projection = 'polar')
 
-iT = 0
+iT = 20
 pot2d = potential[iT]
 eflux2d = eflux[iT]
-print(times[iT])
 
 norm = cm.colors.Normalize(vmax=mini, vmin=maxi)
 if (mini >= 0):
@@ -57,5 +49,5 @@ cax = ax.pcolor(theta, r, eflux2d, \
 ax.contour(theta, r, pot2d, 6, colors = 'k')
 cbar = fig.colorbar(cax)
 
-fig.savefig('test.png')
+fig.savefig('netcdf_test.png')
 plt.close()
--- python/electrodynamics_write.py
+++ python/electrodynamics_write.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
-import netCDF4 as nc
 import datetime as dt
 import numpy as np
 from datetime import datetime
 from datetime import timedelta
+from amie_routines import *
 
 # ------------------------------------------------------------------------
 # make the OCFLB
 # ------------------------------------------------------------------------
 
-def make_ocflb(mlts, bz):
+def make_ocflb(mlts, by, bz, DoAddVar, phase):
 
     mltsR = mlts * np.pi / 12.0
 
     # This is the shift of the ocflb as you go around in MLT, with
     # midnight being deflected by this much towards the equator,
     # and noon being deflected this much towards the pole:
+
     ocflb_lat_deflection_amp = 3.0
 
     # Use studies of cusp latitude to figure out where to put OCFLB
@@ -29,7 +30,26 @@ def make_ocflb(mlts, bz):
             CuspLat = 21.7 * np.exp(0.1*bz) + 58.2
 
     ocflb0 = CuspLat - ocflb_lat_deflection_amp 
-    ocflb = ocflb0 - ocflb_lat_deflection_amp * np.cos(mltsR)
+
+    # Add some variability
+    by_deflection_amp = 0.0
+    
+    if (DoAddVar):
+        magnitude_by = 1.5 * np.abs(np.sin(np.arctan2(by, bz)))
+        if (by > 0.0):
+            loc = 8.0
+        else:
+            loc = 16.0
+        HalfWidth = 3.0
+
+        Period = 1000.0 / (111.0 * np.cos(ocflb0*np.pi/180.0) * 15.0)
+        
+        by_deflection_amp = magnitude_by * np.cos(2*np.pi * mlts/Period + phase)
+        by_deflection_amp = by_deflection_amp * np.exp(-np.abs(mlts-loc)/HalfWidth)
+        
+    ocflb = ocflb0 \
+        - ocflb_lat_deflection_amp * np.cos(mltsR) \
+        + by_deflection_amp
 
     return ocflb
     
@@ -37,10 +57,8 @@ def make_ocflb(mlts, bz):
 # make the aurora
 # ------------------------------------------------------------------------
 
-def make_electron_aurora(mlts, lats, by, bz):
+def make_electron_aurora(mlts, lats, by, bz, ocflbBase):
 
-    ocflbBase = make_ocflb(mlts, bz)
-    
     amp_eflux = 2.0
     amp_avee = 2.5
 
@@ -49,7 +67,7 @@ def make_electron_aurora(mlts, lats, by, bz):
 
     mltsR = mlts * np.pi / 12.0
     r = 90.0 - lats
-    r2d, t2d = np.meshgrid(r, mltsR)
+    t2d, r2d = np.meshgrid(mltsR, r)
     eflux = amp_eflux * (np.cos(t2d)+2.0)/3.0
     avee = amp_avee * (np.cos(t2d)+5.0)/6.0
 
@@ -59,14 +77,14 @@ def make_electron_aurora(mlts, lats, by, bz):
     tau = np.zeros(nLats)
     for i, ocflb in enumerate(ocflbBase):
         tau_ef = tau_eflux_mlts[i]
-        dist = lats - ocflb
+        dist = lats - (ocflb - tau_ef)
         tau[dist >= 0.0] = tau_eflux_mlts[i]
         tau[dist < 0.0] = tau_eflux_mlts[i] * 2.0
         fac = np.exp(-abs(dist**4/tau**4))
-        eflux[i,:] = eflux[i,:] * fac
+        eflux[:,i] = eflux[:,i] * fac
 
         fac = np.exp(-abs(dist**2/tau_avee**2))
-        avee[i,:] = avee[i,:] * fac
+        avee[:,i] = avee[:,i] * fac
         
     return eflux, avee
     
@@ -74,9 +92,8 @@ def make_electron_aurora(mlts, lats, by, bz):
 # make potential
 # ------------------------------------------------------------------------
 
-def make_potential(mlts, lats, by, bz):
+def make_potential(mlts, lats, by, bz, ocflbBase):
 
-    ocflbBase = make_ocflb(mlts, bz)
     ocflb0 = np.mean(ocflbBase)
     
     # Do everything is kV, then transform to V at end
@@ -90,13 +107,14 @@ def make_potential(mlts, lats, by, bz):
     nMlts = len(mlts)
     nLats = len(lats)
     
-    potential = np.zeros([nMlts, nLats])
+    #potential = np.zeros([nMlts, nLats])
+    potential = np.zeros([nLats, nMlts])
 
     # By drives a single cell that is centered at a location that
     # moves as a function of By. So, need to:
     # 1. Redefine the grid in cartesian space:
     r = 90.0 - lats
-    r2d, t2d = np.meshgrid(r, mltsR)
+    t2d, r2d = np.meshgrid(mltsR, r)
     x2d = r2d * np.cos(t2d)
     y2d = r2d * np.sin(t2d)
 
@@ -124,16 +142,16 @@ def make_potential(mlts, lats, by, bz):
         tau[dist < 0.0] = tau_potential_base / 1.5
         fac = np.exp(-abs(dist/tau))
 
-        potential_visc[i,:] = potential_visc[i,:] * fac
+        potential_visc[:,i] = potential_visc[:,i] * fac
 
         if ((by > 0) & (mlts[i] > 12)):
             fac = fac * by_sharpen
         if ((by < 0) & (mlts[i] < 12)):
             fac = fac * by_sharpen
-        potential_bz[i,:] = potential_bz[i,:] * fac
+        potential_bz[:,i] = potential_bz[:,i] * fac
 
         fac = np.exp(-2.0 * dist**2 / tau**2)
-        potential_harang[i,:] = potential_harang[i,:] * fac
+        potential_harang[:,i] = potential_harang[:,i] * fac
         
     potential = (potential_by + \
                  potential_bz + \
@@ -147,77 +165,106 @@ def make_potential(mlts, lats, by, bz):
 # main code
 # ------------------------------------------------------------------------
 
-file = 'test.nc'
+DoAddVarOCFLB = 0
 
-bz = [-2.0, -2.0, -2.0, 0.0]
-by = [-5.0, 0.0,  5.0,  5.0]
+dt = 1.0/60.0
+subtimes = np.arange(12,13+dt,dt)
+nTimes = len(subtimes)
+bysub = np.random.normal(-5.0,2.0,nTimes)
+bzsub = bysub * 0.0 - 2.0
+
+bz = np.concatenate([[-2.0], bzsub, [0.0]])
+by = np.concatenate([[-5.0], bysub,  [-5.0]])
+alltimes = np.concatenate([[0.0], subtimes, [24.0]])
 
 time_1965 = datetime(1965, 1, 1, 0, 0, 0)
-
 basetime = datetime(2020, 3, 20, 0, 0, 0)
-alltimes = np.array([0.0, 12.0, 12.1667, 24.0]) * 3600.0
+alltimes = np.array(alltimes) * 3600.0
 
 nTimes = len(bz)
 
-dLat = 1.0
+dLat = 0.25
 minLat = 40.0
 nLats = (90.0 - minLat)/dLat + 1
-dMlt = 0.5
+dMlt = 0.25
 nMlts = (24.0-0.0)/dMlt + 1
-
-ds = nc.Dataset(file, 'w', format = 'NETCDF4')
-
-cTime = 'Time'
-
-cLat = 'MagneticLatitude'
-cMlt = 'MagneticLocalTime'
-cPot = 'Potential'
-cEFlux = 'EFlux'
-cAveE = 'AveE'
-cBx = 'IMFBx'
-cBy = 'IMFBy'
-cBz = 'IMFBz'
-cFloat4 = 'f4'
-cInt2 = 'i2'
-
-time = ds.createDimension(cTime, None)
-lat = ds.createDimension(cLat, nLats)
-mlt = ds.createDimension(cMlt, nMlts)
-
-timesNC = ds.createVariable(cTime, 'f8', (cTime, ))
-timesNC.units = 's'
-
-latsNC = ds.createVariable(cLat, cFloat4, (cLat, ))
-latsNC.units = 'deg'
-mltsNC = ds.createVariable(cMlt, cFloat4, (cMlt, ))
-mltsNC.units = 'hours'
-
-potentialNC = ds.createVariable(cPot, cFloat4, (cTime, cMlt, cLat, ))
-potentialNC.units = 'Volts'
-
-efluxNC = ds.createVariable(cEFlux, cFloat4, (cTime, cMlt, cLat, ))
-efluxNC.units = 'ergs/cm2/s'
-aveeNC = ds.createVariable(cAveE, cFloat4, (cTime, cMlt, cLat, ))
-aveeNC.units = 'keV'
 
 lats = np.arange(minLat, 90.0+dLat, dLat)
 mlts = np.arange(0.0, 24.0+dMlt, dMlt)
 
-latsNC[:] = lats
-mltsNC[:] = mlts
 
-timearray = [] 
+data = {}
+
+data["nLats"] = nLats
+data["nMlts"] = nMlts
+data["nTimes"] = nTimes
+
+data["lats"] = lats
+data["mlts"] = mlts
+
+data["times"] = []
+for t in alltimes:
+    data["times"].append(basetime + timedelta(seconds = t))
+
+data["imf"] = []
+data["ae"] = []
+data["dst"] = []
+data["hp"] = []
+data["cpcp"] = []
+
+data["Vars"] = ['Potential (kV)', \
+                'Total Energy Flux (ergs/cm2/s)', \
+                'Mean Energy (ergs)']
+
+data["nVars"] = len(data["Vars"])
+
+data["version"] = 1.2
+
+for var in data["Vars"]:
+    data[var] = []
+
+vel_km = 0.4 # km/s
+vel_rad = vel_km / (111.0 * np.cos(70.0*np.pi/180.0)) * np.pi / 180.0
+
 for i in np.arange(0,nTimes):
-    pot2d = make_potential(mlts, lats, by[i], bz[i])
-    eflux2d,avee2d = make_electron_aurora(mlts, lats, by[i], bz[i])
-    potentialNC[i,:,:] = pot2d
-    efluxNC[i,:,:] = eflux2d
-    aveeNC[i,:,:] = avee2d
+
     time_current = basetime + timedelta(seconds = alltimes[i])
+    data["times"].append(time_current)
+
     time_delta = time_current - time_1965
-    timearray.append(time_delta.total_seconds())
+    ut = time_delta.total_seconds() % 86400.0
+    phase = vel_rad * ut
+    
+    ocflb = make_ocflb(mlts, by[i], bz[i], DoAddVarOCFLB, phase)
+    pot2d = make_potential(mlts, lats, by[i], bz[i], ocflb)
+    eflux2d,avee2d = make_electron_aurora(mlts, lats, by[i], bz[i], ocflb)
 
-timesNC[:] = timearray
-print(timearray)
+    #  ; IMF should be (nTimes,4) - V, Bx, By, Bz
+    #  ; AE  (nTimes, 4) - AL, AU, AE, AEI?
+    #  ; Dst (nTimes, 2) - Dst, Dsti?
+    #  ; Hpi (nTimes, 2) - HP, Joule Heating (GW)
+    #  ; CPCP (nTimes) - CPCP (kV)
 
-ds.close()
+    data["imf"].append([400.0, 0.0, by[i], bz[i]])
+    # These are all bad values for now....
+    data["ae"].append([0.0, 0.0, 0.0, 0.0])
+    data["dst"].append([0.0, 0.0])
+    data["hp"].append([0.0, 0.0])
+    data["cpcp"].append((np.max(pot2d) - np.min(pot2d))/1000.0)
+
+    cPot = data["Vars"][0]
+    data[cPot].append(pot2d)
+    
+    cEFlux = data["Vars"][1]
+    data[cEFlux].append(eflux2d)
+    
+    cAveE = data["Vars"][2]
+    data[cAveE].append(avee2d)
+
+# Write out in new netCDF format:
+file = 'test_ed.nc'
+amie_write_netcdf(file, data)
+
+# Write out in old AMIE format:
+file = 'test_ed.bin'
+amie_write_binary(file, data)
