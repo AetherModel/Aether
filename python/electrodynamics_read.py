1fef3887d2413ea9785e1d7d79fd1a32f62f293c
Aaron Ridley <ridley@umich.edu>
2021-04-29 16:56:11 -0400

    FEAT: Use OMNI data to get IMF and solar wind to drive electrodynamics.

--- python/electrodynamics_read.py
+++ python/electrodynamics_read.py
@@ -16,7 +16,7 @@ ncfile = nc.Dataset(file, 'r')
 
 lats = np.array(ncfile.variables['MagneticLatitude'])
 mlts = np.array(ncfile.variables['MagneticLocalTime'])
-potential = np.array(ncfile.variables['Potential'])
+potential = np.array(ncfile.variables['Potential'])/1000.0
 eflux = np.array(ncfile.variables['EFlux'])
 times_in_s = np.array(ncfile.variables['Time'])
 
@@ -28,15 +28,15 @@ for t in times_in_s:
 
 theta, r = np.meshgrid(mlts * np.pi/12.0 - np.pi/2.0, 90.0 - lats)
 
+nLevels = 21
+maxp = np.max(np.abs(potential))
+dr = np.ceil(maxp/(nLevels-1)*2)
+minp = -dr * (nLevels-1)/2
+maxp = np.abs(minp)
+levelp = np.arange(minp,maxp+dr,dr)
+
 maxi = np.max(np.abs(eflux))
 mini = 0.0
-
-fig = plt.figure(figsize = (10,10))
-ax = fig.add_subplot(projection = 'polar')
-
-iT = 20
-pot2d = potential[iT]
-eflux2d = eflux[iT]
 
 norm = cm.colors.Normalize(vmax=mini, vmin=maxi)
 if (mini >= 0):
@@ -44,10 +44,29 @@ if (mini >= 0):
 else:
     cmap = cm.bwr
 
-cax = ax.pcolor(theta, r, eflux2d, \
-                vmin = mini, vmax = maxi, cmap = cmap)
-ax.contour(theta, r, pot2d, 6, colors = 'k')
-cbar = fig.colorbar(cax)
+di = int(np.ceil(nTimes/9))
+
+fig = plt.figure(figsize = (10,10))
+
+iPlot = 1
+ax = []
+cax = []
+for iT in np.arange(0,nTimes,di):
+
+    subplot = 330 + iPlot
+    iPlot += 1
+    print(subplot, iT)
+    ax.append(fig.add_subplot(subplot, projection = 'polar'))
+
+    pot2d = potential[iT]
+    eflux2d = eflux[iT]
+
+    eflux2d[eflux2d < 0.1] = np.nan
+
+    cax.append(ax[-1].pcolor(theta, r, eflux2d, \
+                             vmin = mini, vmax = maxi, cmap = cmap))
+    ax[-1].contour(theta, r, pot2d, levelp, colors = 'k')
+    #fig.colorbar(cax[-1])
 
 fig.savefig('netcdf_test.png')
 plt.close()
