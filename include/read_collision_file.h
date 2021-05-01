ef8442cf0c5af83713e28457d042b93765c14788
Aaron Ridley <ridley@umich.edu>
2021-05-01 16:35:51 -0400

    FEAT: Read in the collision (NU_in, resonant, etc.) file

---
+++ include/read_collision_file.h
@@ -0,0 +1,25 @@
+// Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
+// Full license can be found in License.md
+
+#ifndef INCLUDE_COLLISION_H_
+#define INCLUDE_COLLISION_H_
+
+#include "../include/aether.h"
+
+void read_collision_file(Neutrals &neutrals,
+			 Ions &ions,
+			 Inputs input,
+			 Report &report);
+
+void parse_nu_in_table(std::vector<std::vector<std::string>> csv,
+		       Neutrals &neutrals,
+		       Ions &ions,
+		       Report &report);
+
+void parse_resonant_nu_in_table(std::vector<std::vector<std::string>> csv,
+				Neutrals &neutrals,
+				Ions &ions,
+				Report &report);
+
+#endif  // INCLUDE_IONS_H_
+
