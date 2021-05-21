20905d5c85584f3241a0a6c67c36fbcd77e4fbbf
Aaron Ridley <ridley@umich.edu>
2021-05-21 15:48:12 -0400

    FEAT: Add configuration script to allow users to have more control on what is compiled.

---
+++ Config.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import argparse
+from glob import glob
+import os
+
+# ----------------------------------------------------------------------
+# Function to parse input arguments
+# ----------------------------------------------------------------------
+
+def parse_args(oss):
+
+    parser = argparse.ArgumentParser(description = 'Aether Configure Script')
+
+    parser.add_argument('-netcdf', \
+                        help='Set Aether to use netCDF output', \
+                        action='store_true')
+
+    parser.add_argument('--verbose', '-v', \
+                        help='Turn on Verbose', \
+                        action='store_true')
+    
+    parser.add_argument('-os', \
+                        help='Set OS to use (copies correct Makefile.OS)', \
+                        choices = oss, \
+                        default='none')
+
+    args = parser.parse_args()
+    
+    return args
+
+
+
+# ----------------------------------------------------------------------
+# Execute a command
+# ----------------------------------------------------------------------
+
+def execute_command(command, verbose):
+
+    if (verbose):
+        print(command)
+    os.system(command)
+
+# ----------------------------------------------------------------------
+# Main Code
+# ----------------------------------------------------------------------
+
+makefiles = glob('src/Makefile.*')
+oss = []
+for file in makefiles:
+    oper = file[13:]
+    if (oper != 'OS'):
+        oss.append(oper)
+
+args = parse_args(oss)
+
+print(args.netcdf)
+print(args.os)
+print(args.verbose)
+
+output_dir = "src"
+output_target = "output.cpp"
+if (args.netcdf):
+    output_source = "output_netcdf.cpp"
+else:
+    output_source = "output_binary.cpp"
+
+command = "cd " + output_dir + " ; /bin/rm -f " + output_target + " ; cd -"
+execute_command(command, args.verbose)
+
+command = "cd " + output_dir + \
+    " ; ln -s " + output_source + " " + output_target + " ; cd -"
+execute_command(command, args.verbose)
+
