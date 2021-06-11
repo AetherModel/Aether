a73c7abdc9afa4f247121840fd9ee22a9d4dfd3d
Aaron Ridley <ridley@umich.edu>
2021-06-11 14:28:55 -0400

    Just updated a bit.

--- python/test_electrodynamics.sh
+++ python/test_electrodynamics.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # test the electrodynamics.
-#./electrodynamics_write.py -startdate=20130316 -enddate=20130317 -dt=1 -real -outfile=b20130316n_omni.bin
-#./amie_read_binary.py b20130316n_omni.bin
-#
-#./electrodynamics_write.py -startdate=20130316 -enddate=20130317 -dt=1 -real -outfile=b20130316s_omni.bin -south
-#./amie_read_binary.py b20130316s_omni.bin
-#
-./electrodynamics_write.py -startdate=20130317 -enddate=20130318 -dt=1 -real -ions -outfile=b20130317n_omni_ions.bin
-./amie_read_binary.py b20130317n_omni_ions.bin
+./electrodynamics_write.py -startdate=20110320 -enddate=20110321 -dt=60 -real -outfile=b20110320n_omni.bin
+./amie_read_binary.py -step=1 b20110320n_omni.bin
 
-./electrodynamics_write.py -startdate=20130317 -enddate=20130318 -dt=1 -real -ions -outfile=b20130317s_omni.bin -south
-./amie_read_binary.py b20130317s_omni.bin
+./electrodynamics_write.py -startdate=20110320 -enddate=20110321 -dt=60 -real -south -outfile=b20110320s_omni.bin
+./amie_read_binary.py -step=1 b20110320s_omni.bin
 
+#./electrodynamics_write.py -startdate=20130317 -enddate=20130318 -dt=1 -real -ions -outfile=b20130317n_omni_ions.bin
+#./amie_read_binary.py b20130317n_omni_ions.bin
+#
+#./electrodynamics_write.py -startdate=20130317 -enddate=20130318 -dt=1 -real -ions -outfile=b20130317s_omni.bin -south
+#./amie_read_binary.py b20130317s_omni.bin
+#
 #./electrodynamics_write.py -startdate=20130318 -enddate=20130319 -dt=1 -real -outfile=b20130318n_omni.bin
 #./amie_read_binary.py b20130318n_omni.bin
 #
