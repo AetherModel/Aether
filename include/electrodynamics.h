cfae884501a68ab3ed83cb01ed88264235756df3
Aaron Ridley <ridley@umich.edu>
2021-08-20 09:02:32 -0400

    FEAT: read in electrodynamics (potential and aurora) file and use.

---
+++ include/electrodynamics.h
@@ -0,0 +1,448 @@
+// Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
+// Full license can be found in License.md
+
+#ifndef INCLUDE_ELECTRODYNAMICS_H_
+#define INCLUDE_ELECTRODYNAMICS_H_
+
+/**************************************************************
+ * This is the electrodynamics class for Aether.
+
+   To use this, you have to:
+   1. initialize it, which reads in the electrodynamics file (if used).
+   2. set the time to use.
+   3. set any indices to you need for the electrodynmics.
+   4. set the magnetic latitudes (2d) you need
+   5. set the magnetic local times (2d) you need
+   6. call get_potential or get_eflux or ... to get the electrodynamics
+      on the grid that was specified.
+   2-6 can all be called as many times as you want.
+ *
+ **************************************************************/
+
+#include <vector>
+
+#include "aether.h"
+
+class Electrodynamics {
+
+ public:
+
+  /**************************************************************
+     \brief Initialize electrodynamics variables and routines
+
+     This does the following:
+     - initialize all variables to missing values
+     - read in file if it exists
+
+     \param input Need to pass Input class, so code can get info
+                  about how user has configured things. Inside
+                  input, the function uses .efield_model, .auroral_model,
+                  .electrodynamics_file.
+
+     \param report Need to pass Report class, so reporting can occur  
+   **/
+  Electrodynamics(Inputs input, Report &report);
+
+  /**************************************************************
+     \brief used in main.cpp to ensure electrodynamics times and
+      aether input times match up. Returns false if times are
+      misaligned, true if they are aligned
+
+     \param inputStartTime input file starting time
+
+     \param inputEndTime input file ending time
+   **/
+
+  bool check_times(double inputStartTime, double inputEndTime);
+  
+  /**************************************************************
+     \brief used in advance.cpp to get potential, eflux, avee
+
+
+     \param magLat magnetic latitude
+
+     \param magLocalTime magnetic local time
+
+     \param report reporting
+   **/
+
+  std::tuple<fcube, fmat, fmat> get_electrodynamics(fcube magLat, fcube magLocalTime, Report &report);
+
+  /**************************************************************
+     \brief Gets interpolation indices
+
+     Performs 2d interpolation over search vector to get indices
+
+     \param vals the 2d array that needs indices
+     \param search The vector of values to interpolate over  
+   **/
+
+  fmat get_interpolation_indices(fmat vals, fvec search);
+
+  /**************************************************************
+     \brief Sets time needed for electrodynamics
+
+     Internally, if there is a file read, this function:
+     - finds interpolation indices in time
+     - interpolates the primaary quantities (pot, avee, eflux, etc) to
+       the given time, creating internal states of these quantities. This
+       is done for all relavant grids.
+
+     \param time the time requested.
+     \param report Need to pass Report class, so reporting can occur  
+   **/
+  
+  void set_time(double time, Report &report);
+
+  /**************************************************************
+     \brief Sets the current grid to request data on
+
+     Internally, if there is a file read, this function finds the
+     interpolation indices in space for each relevant grid
+
+     \param lats a 2D matrix of magnetic latitudes to interpolate to
+     \param mlts a 2D matrix of magnetic local times to interpolate to
+     \param report Need to pass Report class, so reporting can occur  
+   **/
+  void set_grid(fmat lats, fmat mlts, Report &report);
+
+  /**************************************************************
+     \brief Set the IMF Bx for internal usage
+     \param value Value to assign to IMF Bx (nT)
+   **/
+  void set_imf_bx(float value);
+
+  /**************************************************************
+     \brief Set the IMF By for internal usage
+     \param value Value to assign to IMF By (nT)
+   **/
+  void set_imf_by(float value);
+
+  /**************************************************************
+     \brief  Set the IMF Bz for internal usage
+     \param value Value to assign to IMF Bz (nT)
+   **/
+  void set_imf_bz(float value);
+
+  /**************************************************************
+     \brief Set the Solar Wind Velocity for internal usage
+     \param value Value to assign to Solar wind velocity (km/s)
+   **/
+  void set_sw_v(float value);
+
+  /**************************************************************
+     \brief Set the Solar Wind Density for internal usage
+     \param value Value to assign to Solar Wind Density (/cc)
+   **/
+  void set_sw_n(float value);
+
+  /**************************************************************
+     \brief Set the Hemispheric Power for internal usage
+     \param value Value to assign to Hemispheric Power (GW)
+     \param
+   **/
+  void set_hp(float value);
+
+  /**************************************************************
+     \brief Set the AU index for internal usage
+     \param value Value to assign to Auroral Upper Index (nT)
+   **/
+  void set_au(float value);
+
+  /**************************************************************
+     \brief Set the AL index for internal usage
+     \param value Value to assign to Auroral Lower Index (nT)
+   **/
+  void set_al(float value);
+
+  /**************************************************************
+     \brief Set the AE Index for internal usage
+     \param value Value to assign to Auroral Electrojet Index (nT)
+   **/
+  void set_ae(float value);
+
+  /**************************************************************
+     \brief Set the Kp Index for internal usage
+     \param value Value to assign to Kp index
+   **/
+  void set_kp(float value);
+  
+  /**************************************************************
+     \brief Get 2D electric potential on specified grid
+
+     This function returns the electric potential on the requested
+     grid at the requested time (with the requested indices, if
+     applicable)
+
+     This function does the following:
+     - creates an empty potential matrix ok, I see to return
+     - Loops through the grids in priority order calling set_values
+       with the potentials in the grids
+
+     \param report Need to pass Report class, so reporting can occur  
+   **/
+  fcube get_potential(fcube magLat, fcube magLocalTime, Report &report);
+
+  /**************************************************************
+     \brief Get 2D electron energy flux on specified grid
+
+     This function returns the electron energy flux on the requested
+     grid at the requested time (with the requested indices, if
+     applicable)
+
+     This function does the following:
+     - creates an empty eflux matrix to return
+     - Loops through the grids in priority order calling set_values
+       with the eflux in the grids
+
+     \param report Need to pass Report class, so reporting can occur  
+   **/
+  fmat get_eflux(fcube magLat, fcube magLocalTime, Report &report);
+
+  /**************************************************************
+     \brief Get 2D electron average energy on specified grid
+
+     This function returns the electron average energy on the requested
+     grid at the requested time (with the requested indices, if
+     applicable)
+
+     This function does the following:
+     - creates an empty avee matrix to return
+     - Loops through the grids in priority order calling set_values
+       with the avee in the grids
+
+     \param report Need to pass Report class, so reporting can occur  
+   **/
+  fmat get_avee(fcube magLat, fcube magLocalTime, Report &report);
+
+  /**************************************************************
+     \brief Get 2D ion energy flux on specified grid
+
+     This function returns the ion energy flux on the requested
+     grid at the requested time (with the requested indices, if
+     applicable)
+
+     This function does the following:
+     - creates an empty ion eflux matrix to return
+     - Loops through the grids in priority order calling set_values
+       with the ion eflux in the grids
+
+     \param report Need to pass Report class, so reporting can occur  
+   **/
+  fmat get_ion_eflux(Report &report);
+
+  /**************************************************************
+     \brief Get 2D ion average energy on specified grid
+
+     This function returns the ion average energy on the requested
+     grid at the requested time (with the requested indices, if
+     applicable)
+
+     This function does the following:
+     - creates an empty ion avee matrix to return
+     - Loops through the grids in priority order calling set_values
+       with the ion avee in the grids
+
+     \param report Need to pass Report class, so reporting can occur  
+   **/
+  fmat get_ion_avee(Report &report);
+  
+ private:
+
+  /// This is the interpolation method for time:
+  int iTimeInterpolationMethod;
+  /// For all iteration methods, code should return first value if
+  /// before first time, last value if after last time.
+  /// Use the previous value:
+  const int iPrevious_ = 1;
+  /// Use the next value:
+  const int iNext_ = 2;
+  // Use the closest value:
+  const int iClosest_ = 3; 
+  /// Interpolate:
+  const int iInterp_ = 4;
+
+  /// The input file to read that contains the grid of electrodynamics:
+  std::string input_file;
+
+  /// Variables needed by user - all set by set_ functions:
+
+  /// This is the time needed:
+  double time_needed;
+
+  /// A 2d array of magnetic latitude needed. Can set interpolation
+  /// coefficients in all of the grids when this is called:
+  fmat lats_needed;
+
+  /// A 2d array of magnetic local times needed. Can set interpolation
+  /// coefficients in all of the grids when this is called:
+  fmat mlts_needed;
+  
+  /// These are all indices that may be needed by sub-models:
+  float imf_bx_needed;
+  float imf_by_needed;
+  float imf_bz_needed;
+  float sw_v_needed;
+  float sw_n_needed;
+  float hp_needed;
+  float au_needed;
+  float al_needed;
+  float ae_needed;
+  float kp_needed;
+
+  /// If we read in an electrodyanmics file, set this to 1. Otherwise,
+  /// set it to 0:
+  int iUseGridBasedModel;
+
+  /// If we don't read in an electrodynamics file, then this should be
+  /// set to an electric field model to use.  Need to add model types.
+  std::string efield_model_to_use;
+
+  /// If we don't read in an electrodynamics file, then this should be
+  /// set to an auroral model to use.  Need to add model types.
+  std::string auroral_model_to_use;
+  
+  /// interpolation indices, for each one, the integer portion is
+  /// the index, while the decimal part is the percentage of the way
+  /// between the index and index + 1
+  /// for time, we are assuming that all grids have the same times or that
+  /// there are no overlaps in time, I think.
+  float time_index;
+
+  /**************************************************************
+   * input_electrodynamics_struct is a structure that contains
+   * information about the electrodynamics on a magnetic latitude /
+   * local time grid. It is assumed that the quantities are on a
+   * regular grid, in that the lat and mlt can be described with 1D
+   * arrays and the quantities can be described with 2D arrays.
+   * Time then makes these matrices into vectors. The quantities
+   * stored are energy flux, average energy, electric potential,
+   * ion energy flux, ion average energy.
+   **/
+  struct input_electrodynamics_struct {
+
+    /// Number of latitudes and magnetic local times:
+    int nLats;
+    int nMlts;
+
+    /// vectors of magnetic latitudes and magnetic local times:
+    fvec mlats;
+    fvec mlts;
+
+    /// Vector of times (in seconds):
+    std::vector<double> times;
+
+    /// Vector of 2d electric potentials (in Volts):
+    std::vector<fmat> potential;
+    /// potential at current time:
+    fmat potential_current;
+    
+    /// Vector of 2d electron energy flux (in ergs/cm2/s):
+    std::vector<fmat> energy_flux;
+    /// at current time:
+    fmat energy_flux_current;
+    
+    /// Vector of 2d electron average energy (in keV):
+    std::vector<fmat> average_energy;
+    /// average energy at current time:
+    fmat average_energy_current;
+    
+    /// Vector of 2d ion energy flux (in ergs/cm2/s):
+    std::vector<fmat> ion_energy_flux;
+    /// ion energy flux at current time:
+    fmat ion_energy_flux_current;
+    
+    /// Vector of 2d ion average energy (in keV):
+    std::vector<fmat> ion_average_energy;
+    /// ion average energy at current time:
+    fmat ion_average_energy_current;
+
+    /// Set to 1 if ion precipitation is included, else set to 0:
+    int DoesIncludeIonPrecip;
+    
+    /// This sets the priority of the grid. The higher the number, the
+    /// more important it is, so it should overwrite any regions of
+    /// a lower priority grid. For example, you could have a global
+    /// grid and a regional grid, whre the global grid is assigned a
+    /// priority of 1, and the regional grid is assigned a priority of 2,
+    /// so that the regions inside the regional grid will overwrite
+    /// the global grid:
+    int grid_priority;
+
+    /// interpolation indices for latitudes. If the requested latitude
+    /// is outside of the latitude range of the grid, then the
+    /// interpolation index should be set to -1:
+    fmat lats_indices;
+    /// interpolation indices for mlts. If the requested mlt
+    /// is outside of the mlt range of the grid, then the
+    /// interpolation index should be set to -1:
+    fmat mlts_indices;
+    
+  };
+  
+  /// As described above, a structure containing the grid-based
+  /// values of electrodynamics as a function of time.  This is
+  /// vector, because we can have nested grids, or, in theory, the
+  /// grid could change as a function of time. You can then search
+  /// for the apropriate grid in space and time.
+  std::vector<input_electrodynamics_struct> input_electrodynamics;
+  
+  /// Because each grid has a priority, we need to go through them in
+  /// priority order, this is the sorted indices list, so that
+  /// grid_order[0] points to the input_electrodynamics with the
+  /// lowest priority, grid_order[1] points to the 2nd lowest, etc.
+  std::vector<int> grid_order;
+  
+  /// Number of input grids for electrodynamics:
+  int nElectrodynamicsGrids;
+  
+  /**************************************************************
+     \brief Reads a netcdf file that has the electrodynamics specification
+
+     Reads a netcdf file that contains at a minimum:
+     - Potential
+     - Auroral energy flux
+     - Auroral average energy
+     May contain:
+     - Ion energy flux
+     - Ion average energy
+
+     These are on a magnetic lat/mlt grid as a function of time, and
+     should be put into the input_electrodynamics structure.
+
+     \param filename
+     \param report Need to pass Report class, so reporting can occur
+   **/
+  void read_netcdf_electrodynamics_file(std::string filename,
+					Report &report);
+
+  /**************************************************************
+     \brief Takes the pot/eflux/avee/etc and interpolates to the grid
+
+     This function takes values from the input_electrodynamics
+     structure and interpolates the values onto the user specified
+     grid. The tricky bit is that there can be multiple overlapping
+     grids.  So, this needs to be called for each of the existing
+     grids, so that the values are overwritten. To keep it
+     "functional", we pass in the last round of values and those are
+     moved into the output values and then the overlapping region is
+     overwritten (e.g., in the get_potential function, the 
+     grids need to be cycled through calling get_values with the
+     potential on that grid and the interpolation indices for the grid.
+
+     \param values_current the pot/eflux/avee/etc from
+     input_electrodynamics grid
+ 
+     \param lats_indices the interpolation indices for the current
+     grid latitudes
+
+     \param mlts_indices the interpolation indices for the current
+     grid mlts
+
+     \param values_old the output of this function for the last grid
+  **/  
+  
+  fmat get_values(fmat matToInterpolateOn, int rows, int cols);
+};
+
+#endif // INCLUDE_ELECTRODYNAMICS_H_
---
+++ src/electrodynamics.cpp
@@ -0,0 +1,321 @@
+
+#include <iostream>
+#include <string>
+#include <netcdf>
+
+#include "../include/aether.h"
+#include <vector>
+
+// Initialize Electrodynamics
+
+Electrodynamics::Electrodynamics(Inputs input, Report &report) {
+  read_netcdf_electrodynamics_file(input.get_electrodynamics_file(), report);
+}
+
+fcube Electrodynamics::get_potential(fcube magLat,
+                                     fcube magLocalTime,
+                                     Report &report) {
+  fcube pot(magLat.n_rows, magLat.n_cols, magLat.n_slices);
+  pot.zeros();
+  int time_pos = static_cast<int>(time_index);
+  fmat e_potentials = input_electrodynamics[0].potential[time_pos];
+
+  for (int i = 0; i < magLat.n_slices; ++i) {
+    set_grid(magLat.slice(i) * cRtoD, magLocalTime.slice(i), report);
+    pot.slice(i) = get_values(e_potentials, magLat.n_rows, magLat.n_cols);
+  }
+
+  return pot;
+}
+
+fmat Electrodynamics::get_eflux(fcube magLat,
+                                fcube magLocalTime,
+                                Report &report) {
+  int i = magLat.n_slices - 1;
+  set_grid(magLat.slice(i) * cRtoD, magLocalTime.slice(i), report);
+  int time_pos = static_cast<int>(time_index);
+  fmat e_e_flux = input_electrodynamics[0].energy_flux[time_pos];
+  return get_values(e_e_flux, magLat.n_rows, magLat.n_cols);
+}
+
+fmat Electrodynamics::get_avee(fcube magLat,
+                               fcube magLocalTime,
+                               Report &report) {
+  int i = magLat.n_slices - 1;
+  set_grid(magLat.slice(i) * cRtoD, magLocalTime.slice(i), report);
+  int time_pos = static_cast<int>(time_index);
+  fmat e_avee = input_electrodynamics[0].average_energy[time_pos];
+  return get_values(e_avee, magLat.n_rows, magLat.n_cols);
+}
+
+fmat Electrodynamics::get_values(fmat matToInterpolateOn, int rows, int cols) {
+  fmat slice(rows, cols);
+  slice.zeros();
+
+  for (int r = 0; r < rows; ++r) {
+    for (int c = 0; c < cols; ++c) {
+      float h_pos = input_electrodynamics[0].mlts_indices(r, c);
+      float v_pos = input_electrodynamics[0].lats_indices(r, c);
+      int r_start, c_start;
+
+      if (h_pos < 0)
+        c_start = 0;
+      else if (h_pos >= matToInterpolateOn.n_cols - 1)
+        c_start = matToInterpolateOn.n_cols - 2;
+      else
+        c_start = h_pos;
+
+      if (v_pos < 0)
+        r_start = 0;
+      else if (v_pos >= matToInterpolateOn.n_rows - 1)
+        r_start = matToInterpolateOn.n_rows - 2;
+      else
+        r_start = v_pos;
+
+      float first_row_slope =
+        matToInterpolateOn(r_start, c_start + 1) -
+        matToInterpolateOn(r_start, c_start);
+      float second_row_slope =
+        matToInterpolateOn(r_start + 1, c_start + 1) -
+        matToInterpolateOn(r_start + 1, c_start);
+      float first_row_val =
+        matToInterpolateOn(r_start, c_start) +
+        first_row_slope * (h_pos - c_start);
+      float second_row_val =
+        matToInterpolateOn(r_start + 1, c_start) +
+        second_row_slope * (h_pos - c_start);
+      //time to vertically linear interpolate these two values
+      float vertical_slope = second_row_val - first_row_val;
+      float final_value =
+        first_row_val + vertical_slope * (v_pos - r_start);
+      slice(r, c) = final_value;
+    }
+  }
+
+  return slice;
+}
+
+//average energy and eflux energy as well call to get_values
+
+std::tuple<fcube, fmat, fmat> Electrodynamics::get_electrodynamics(fcube magLat,
+    fcube magLocalTime,
+    Report &report) {
+  fcube pot;
+  fmat eflux;
+  fmat avee;
+
+  if (!input_electrodynamics.empty()) {
+    pot = get_potential(magLat, magLocalTime, report);
+    eflux = get_eflux(magLat, magLocalTime, report);
+    avee = get_avee(magLat, magLocalTime, report);
+  } else {
+    pot.set_size(magLat.n_rows, magLat.n_cols, magLat.n_slices);
+    pot.zeros();
+    eflux.set_size(magLat.n_rows, magLat.n_cols);
+    eflux.zeros();
+    avee.set_size(magLat.n_rows, magLat.n_cols);
+    avee.ones();
+  }
+
+  return std::make_tuple(pot, eflux, avee);
+}
+
+bool Electrodynamics::check_times(double inputStartTime, double inputEndTime) {
+  std::vector<double> e_times = input_electrodynamics[0].times;
+  int iLow = 0;
+  int iHigh = e_times.size() - 1;
+  return !(inputStartTime > e_times[iHigh] || inputEndTime < e_times[iLow]);
+}
+
+void Electrodynamics::set_time(double time, Report &report) {
+  std::string function = "Electrodynamics::set_time";
+  static int iFunction = -1;
+  //report.enter(function, iFunction);
+
+  time_needed = time;
+
+  int64_t iLow, iMid, iHigh, N;
+  double interpolation_index, x, dt;
+  double intime = time;
+  std::vector<double> times = input_electrodynamics[0].times;
+  // Check to see if the time is below the bottom time in the vector:
+  iLow = 0;
+  iHigh = times.size() - 1;
+
+  if (intime <= times[iLow]) {
+    interpolation_index = 0.0;
+    std::cout <<
+              "Warning: current time below first available potential-vector time, using first time"
+              << "\n";
+  } else if (intime >= times[iHigh]) {
+    interpolation_index = iHigh;
+    std::cout <<
+              "Warning: current time above last available potential-vector time, using last time"
+              << "\n";
+  }
+
+  // At this point, we know that it is somewhere between the highest
+  // and lowest values:
+  else {
+    iMid = (iHigh + iLow) / 2;
+
+    while (iHigh - iLow > 1) {
+      // Break if iMid <= time < iMid+1
+      if (times[iMid] == intime)
+        break;
+
+      if (times[iMid] <= intime &&
+          times[iMid + 1] > intime)
+        break;
+
+      // Upper Half:
+      if (times[iMid] < intime) {
+        iLow = iMid;
+        iMid = (iHigh + iLow) / 2;
+      } else {
+        iHigh = iMid;
+        iMid = (iHigh + iLow) / 2;
+      }
+    }
+
+    // At this point, time should be between iMid and iMid+1:
+
+    dt = (times[iMid + 1] - times[iMid]);
+    x = (intime - times[iMid]) / dt;
+
+    interpolation_index = iMid + x;
+  }
+
+  time_index = interpolation_index;
+  //report.exit(function);
+}
+
+void Electrodynamics::set_grid(fmat lats, fmat mlts, Report &report) {
+  std::string function = "Electrodynamics::set_grid";
+  static int iFunction = -1;
+  //report.enter(function, iFunction);
+
+  lats_needed = abs(lats);
+  mlts_needed = mlts;
+
+  //uses first input_electrodynamics struct
+  fvec lat_search = input_electrodynamics[0].mlats;
+  fvec mlt_search = input_electrodynamics[0].mlts;
+
+  input_electrodynamics[0].lats_indices = get_interpolation_indices(lats_needed,
+                                          lat_search);
+  input_electrodynamics[0].mlts_indices = get_interpolation_indices(mlts_needed,
+                                          mlt_search);
+
+  // If we have read in a file, we need to create interpolation indices here:
+
+  // This is a bit more complicated, since we need to loop through
+  // all of the points in lats and use the 1d interpolation scheme to find
+  // the index, store, and move to next point; then repeat with mlts.
+
+
+  //report.exit(function);
+}
+
+fmat Electrodynamics::get_interpolation_indices(fmat vals, fvec search) {
+  fmat res(vals.n_rows, vals.n_cols, fill::zeros);
+
+  for (int i = 0; i < vals.n_rows; ++i) {
+    for (int j = 0; j < vals.n_cols; ++j) {
+
+      float in = vals(i, j);
+      int64_t iLow, iMid, iHigh, N;
+      double interpolation_index, x, dx;
+
+
+      // Check to see if the time is below the bottom time in the vector:
+      iLow = 0;
+
+      if (in <= search(iLow))
+        interpolation_index = 0.0;
+
+      // Check to see if the time is above the top time in the vector:
+      iHigh = search.n_rows - 1;
+
+      if (in >= search(iHigh))
+        interpolation_index = iHigh;
+
+      // At this point, we know that it is somewhere between the highest
+      // and lowest values:
+
+      iMid = (iHigh + iLow) / 2;
+
+      while (iHigh - iLow > 1) {
+        // Break if iMid <= time < iMid+1
+        if (search[iMid] == in)
+          break;
+
+        if (search[iMid] <= in &&
+            search[iMid + 1] > in)
+          break;
+
+        // Upper Half:
+        if (search[iMid] < in) {
+          iLow = iMid;
+          iMid = (iHigh + iLow) / 2;
+        } else {
+          iHigh = iMid;
+          iMid = (iHigh + iLow) / 2;
+        }
+      }
+
+      // At this point, time should be between iMid and iMid+1:
+
+      dx = (search[iMid + 1] - search[iMid]);
+      x = (in - search[iMid]) / dx;
+
+      interpolation_index = iMid + x;
+      res(i, j) = interpolation_index;
+    }
+  }
+
+  return res;
+}
+
+
+void Electrodynamics::set_imf_bx(float value) {
+  imf_bx_needed = value;
+}
+
+void Electrodynamics::set_imf_by(float value) {
+  imf_by_needed = value;
+}
+
+void Electrodynamics::set_imf_bz(float value) {
+  imf_bz_needed = value;
+}
+
+void Electrodynamics::set_sw_v(float value) {
+  sw_v_needed = value;
+}
+
+void Electrodynamics::set_sw_n(float value) {
+  sw_n_needed = value;
+}
+
+void Electrodynamics::set_hp(float value) {
+  hp_needed = value;
+}
+
+void Electrodynamics::set_au(float value) {
+  au_needed = value;
+}
+
+void Electrodynamics::set_al(float value) {
+  al_needed = value;
+}
+
+void Electrodynamics::set_ae(float value) {
+  ae_needed = value;
+}
+
+void Electrodynamics::set_kp(float value) {
+  kp_needed = value;
+}
+
+
