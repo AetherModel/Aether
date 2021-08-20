be1810d97f6a3398c627e4e77e91e7e20d2ee8cd
Aaron Ridley <ridley@umich.edu>
2021-08-20 09:20:32 -0400

    FEAT: interpolation methods

--- include/solvers.h
+++ include/solvers.h
@@ -21,4 +21,38 @@ fcube solver_chemistry(fcube density,
 		       fcube loss,
 		       float dt);
 
+/// Use the previous value:
+const int iPrevious_ = 1;
+/// Use the next value:
+const int iNext_ = 2;
+// Use the closest value:
+const int iClosest_ = 3; 
+/// Interpolate:
+const int iInterp_ = 4;
+
+double interpolate_1d_get_index_doubles(double intime,
+					std::vector<double> times);
+
+// Overloading the interpolation function:
+double interpolate_1d_w_index(std::vector<double> values,
+			      double interpolation_index,
+			      int interpolation_type);
+double interpolate_1d_w_index(std::vector<float> values,
+			      double interpolation_index,
+			      int interpolation_type);
+double interpolate_1d_w_index(std::vector<float> values,
+			      float interpolation_index,
+			      int interpolation_type);
+double interpolate_1d_w_index(fvec values,
+			      double interpolation_index,
+			      int interpolation_type);
+fmat interpolate_1d_w_index(std::vector<fmat> values,
+			    double interpolation_index,
+			    int interpolation_type);
+
+fcube calc_gradient_lon(fcube value, Grid grid);
+fcube calc_gradient_lat(fcube value, Grid grid);
+fcube calc_gradient_alt(fcube value, Grid grid);
+std::vector<fcube> calc_gradient_vector(fcube value_scgc, Grid grid);
+  
 #endif  // INCLUDE_SOLVERS_H_
