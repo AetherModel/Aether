0df0f45d20a0d1cd2095b0432df3e08ade2df56f
Aaron Ridley <ridley@umich.edu>
2021-09-23 10:56:27 -0400

    DOC: Added comments.

--- include/read_f107_file.h
+++ include/read_f107_file.h
@@ -4,22 +4,61 @@
 #ifndef INCLUDE_READ_INDICES_H_
 #define INCLUDE_READ_INDICES_H_
 
+/**************************************************************
+ * A set of routines to read in a number of different indices
+ * files, including the NGDC F10.7 files, and OMNIWeb files.
+ **************************************************************/
+
 #include <vector>
 #include <string>
 
-#include "indices.h"
-#include "report.h"
+#include "aether.h"
 
+/**********************************************************************
+  \brief Reads in all of the indices files and stores them in Indices
+
+  This function goes through all of the input indices files and 
+  reads in the files, then stores the values into the Indices class.
+  At this point, it can read in the following file types:
+  1. NGDC F10.7 files.
+  2. OMNIWeb files that include IMF, Solar Wind, and/or AE/AU/AL.
+
+  \param indices the indices class
+  \param args info about how user has configured things
+  \param report allow reporting to occur
+**/
+int read_and_store_indices(Indices &indices, Inputs args, Report &report);
+
+/**********************************************************************
+  \brief Read the NGDC F10.7 file format and store in the index_file struct
+  \param f107_file the f10.7 file to read in
+  \param report allow reporting to occur
+**/
 index_file_output_struct read_f107_file(std::string f107_file,
 					Indices indices,
 					Report &report);
+
+/**********************************************************************
+  \brief Read the OMNIWeb file format and store in the index_file struct
+  \param omni_file the onmiweb file to read
+  \param indices needed to get the indices index for each variable
+  \param report allow reporting to occur
+**/
 index_file_output_struct read_omni_file(std::string omni_file,
 					Indices indices,
 					Report &report);
+
+/**********************************************************************
+  \brief This code compares a string to return the variable index
+  \param variable_name the name of the variable to match with for OMNIWeb files
+  \param indices needed to get the indices index for each variable
+**/
 int pair_omniweb_index(std::string, Indices index);
+
+/**********************************************************************
+  \brief This code returns the missing value for a given OMNIWeb variable
+  \param variable_name the name of the variable to get the OMNIWeb missing value
+**/
 float get_omniweb_missing_value(std::string);
-
-int read_and_store_indices(Indices &indices, Inputs args, Report &report);
-
 
 #endif  // INCLUDE_READ_INDICES_H_
