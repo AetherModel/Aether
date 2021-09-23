2c8171690d9049648bdd9d7c543b3a30c6d8fcb3
Aaron Ridley <ridley@umich.edu>
2021-09-23 10:58:58 -0400

    STY: Changed f107 file read to indices read

--- include/aether.h
+++ include/aether.h
@@ -28,30 +28,63 @@
 #include <armadillo>
 using namespace arma;
 
+// Contains all information about time in the code and wall time:
 #include "times.h"
+
+// Contains a reporting system for the model include verbose level and timing:
 #include "report.h"
+
+// not done
 #include "inputs.h"
+
+// Defines physical and conversion constants
 #include "constants.h"
+
+// Needs to be eliminated (but not right now!)
 #include "sizes.h"
+
+// These are functions that convert time between different systems.
 #include "time_conversion.h"
+
+// Functions that assist in the reading of files.
 #include "file_input.h"
+
+// A class for keeping track of indices (1d vectors w/time)
 #include "indices.h"
-#include "read_f107_file.h"
+
+// Read indices (f107, omni) file types
+#include "read_indices_files.h"
+
+// A class for keeping track of all of the planetary characteristics
 #include "planets.h"
+
+// not done
 #include "grid.h"
 
+// Contains the neutral states and derived quantities
 #include "neutrals.h"
+
+// not done
 #include "ions.h"
 
+// not done
 #include "bfield.h"
 
+// Defines the Extreme Ultraviolet radiation above the atmosphere
 #include "euv.h"
+
+// not done
 #include "calc_euv.h"
+// not done
 #include "chemistry.h"
+// not done
 #include "output.h"
+// not done
 #include "advance.h"
 
+// not done
 #include "solvers.h"
+// not done
 #include "transform.h"
 
 #endif  // INCLUDE_AETHER_H_
