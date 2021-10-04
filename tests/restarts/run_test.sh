c6da5d356431dec1f6289e7729f7c2575e49fc4c
Aaron Ridley <ridley@umich.edu>
2021-10-04 08:46:54 -0400

    BUG: need the 'cd's and lowered plotting altitude.

--- tests/restarts/run_test.sh
+++ tests/restarts/run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -rf run.halves run.whole
+
 cp -R ../../share/run ./run.whole
 cd run.whole
 cp ../aether.whole.json ./aether.json
@@ -7,26 +9,35 @@ cp ../aether.whole.json ./aether.json
 # plot the output
 cd UA/output
 # [O]:
-run_plot_model_results.py -var=3 -alt=350 3DALL_20110320_010000.bin
+run_plot_model_results.py -var=3 -alt=250 3DALL_20110320_010000.bin
 # Tn:
-run_plot_model_results.py -var=14 -alt=350 3DALL_20110320_010000.bin
+run_plot_model_results.py -var=14 -alt=250 3DALL_20110320_010000.bin
 # [e-]
-run_plot_model_results.py -var=23 -alt=350 3DALL_20110320_010000.bin
+run_plot_model_results.py -var=23 -alt=250 3DALL_20110320_010000.bin
+
+cd ../../..
 
 cp -R ../../share/run ./run.halves
 cd run.halves
 # first part of the run
 cp ../aether.first.json ./aether.json
 ./aether
+cd UA ; rm -f restartIn ; cp -R restartOut restartIn ; cd ..
 # second part of the run
 cp ../aether.second.json ./aether.json
 ./aether
 # plot the output
 cd UA/output
 # [O]:
-run_plot_model_results.py -var=3 -alt=350 3DALL_20110320_010000.bin
+run_plot_model_results.py -var=3 -alt=250 3DALL_20110320_010000.bin
 # Tn:
-run_plot_model_results.py -var=14 -alt=350 3DALL_20110320_010000.bin
+run_plot_model_results.py -var=14 -alt=250 3DALL_20110320_010000.bin
 # [e-]
-run_plot_model_results.py -var=23 -alt=350 3DALL_20110320_010000.bin
+run_plot_model_results.py -var=23 -alt=250 3DALL_20110320_010000.bin
 
+cd ../../..
+
+# you can then look at:
+# restarted run results: run.halves/UA/output/*.png
+# whole run results: run.whole/UA/output/*.png
+# using whatever viewer you have
