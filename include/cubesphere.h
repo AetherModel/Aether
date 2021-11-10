f54112fe6e0d62312a711ae3409f68c127878af5
Qusai Al Shidi <me@qalshidi.science>
2021-11-10 14:36:03 -0500

    MAINT: Reorganized to make helper functions.

--- include/cubesphere.h
+++ include/cubesphere.h
@@ -5,41 +5,21 @@
 #include <armadillo>
 
 /*************************************************
- * \brief A class to handle quad sphere grids.
+ * \brief A class to handle quad sphere grid logic.
  *************************************************/
-class CubeSphere {
-  public:
-    /// Constructor of a quad sphere grid with side of one size being \f$ n \f$.
-    CubeSphere(const int n);
-    /// Returns size of one side \f$ n \f$.
-    const unsigned int size_side();
-    /// Returns size of cube sphere \f$ n \times n \times 6 \f$.
-    const unsigned int size();
-    /** Returns x, y and z locations of a cube from -1 to +1.
-     *
-      */
-    const arma_mat map_cube2sphere(const arma_mat& cube) {
-      const arma_mat sphere(arma::size(cube));
-      const arma_vec x2_2 = (cube(0)%cube(0))/2;
-      const arma_vec y2_2 = (cube(1)%cube(1))/2;
-      const arma_vec z2_2 = (cube(2)%cube(2))/2;
 
-      sphere(0) = c2s(cube(0), y2_2, z2_2);
-      sphere(1) = c2s(cube(1), z2_2, x2_2);
-      sphere(2) = c2s(cube(2), x2_2, y2_2);
+/** Returns x, y and z locations of a cube from -1 to +1.
+ **/
+arma_mat map_cube2sphere(const arma_mat& cube);
 
-      return sphere;
-    }
+/** Helper function the transforms cartesian coordinates to cube sphere grid
+ * vertices in cartesian coordinates.
+ */
+arma_vec c2s(const arma_vec& x, const arma_vec& y, const arma_vec& z);
 
-  private:
-    /// The size of one side of a cube sphere.
-    unsigned int n;
+/** Helper function the transforms cartesian coordinates to cube sphere grid
+ * vertices in cartesian coordinates.
+ */
+precision_t cube2sphere_float(const precision_t x, const precision_t y, const precision_t z);
 
-    const arma_vec c2s(const arma_vec& x,
-                       const arma_vec& y2_2,
-                       const arma_vec& z2_2) {
-      return x%(arma::sqrt(arma::ones<arma_vec>-y2_2-z2_2+y2_2%z2_2*(2/3)));
-    }
-};
-
-#endif INCLUDE_CUBESPHERE_H_
+#endif  // INCLUDE_CUBESPHERE_H_
