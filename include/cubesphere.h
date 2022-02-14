d82d1e1ef85f23efa6f5de21578e6fe39d7a5bfe
Qusai Al Shidi <me@qalshidi.science>
2022-02-14 16:08:25 -0500

    FEAT: CubeSphere specifications. Still WIP.

--- include/cubesphere.h
+++ include/cubesphere.h
@@ -5,8 +5,9 @@
 #include <armadillo>
 
 /*************************************************
- * \brief A class to handle quad sphere grid logic.
+ * \brief A namespace withe all quad sphere grid logic.
  *************************************************/
+namespace CubeSphere {
 
 /** Returns x, y and z locations of a cube from -1 to +1.
  **/
@@ -20,6 +21,67 @@ arma_vec cube2sphere(const arma_vec& x, const arma_vec& y, const arma_vec& z);
 /** Helper function the transforms cartesian coordinates to cube sphere grid
  * vertices in cartesian coordinates.
  */
-precision_t cube2sphere_float(const precision_t x, const precision_t y, const precision_t z);
+precision_t cube2sphere(const precision_t& x, const precision_t& y, const precision_t& z);
+
+/** Helper function that transforms cartesian coordinates of cube to cube sphere
+ * using an x, y, z vector as input.
+ */
+arma_vec cube2sphere(const arma_vec& p);
+
+/** Returns normalized x, y, z locations based on number of subdivisions per
+ * face
+ */
+arma_cube sphere_face_locations(const uint& face, const uint& subdivisions);
+
+/** Returns the normalized x, y, z coordinate locations of a cell in the
+ * cubesphere grid.
+ */
+arma_vec cartesian_location(const uint& face, const uint& subdivisions,
+                            const uint& row, const uint& column);
+
+/** Quad Tree Node base class.
+ */
+class QTNode {
+      unsigned short m_pixnum;
+      unsigned int m_resolution = 1;
+      unsigned int m_depth = 0;
+      std::shared_ptr<QTNode> m_children[4];
+      /// Position in cube.
+      arma_vec m_position = {0, 0, 0};
+      /// Center position in cube sphere.
+      arma_vec m_cell_center = {0, 0, 0};
+      // other details to add
+
+      public:
+      /// Constructor
+      QTNode(const uint& face,
+             const arma_vec& position,
+             const uint& resolution,
+             const uint& depth);
+      /// Destructor
+      ~QTNode();
+      /// Returns if the node is a leaf based on resolution.
+      bool is_leaf();
+      };
+
+/// The normalized origins of each face of the cube (i.e centers).
+static const arma_mat ORIGINS = {
+      {-1.0, 0.0, 0.0},
+      {0.0, -1.0, 0.0},
+      {0.0, 0.0, -1.0},
+      {1.0, 0.0, 0.0},
+      {0.0, 1.0, 0.0},
+      {0.0, 0.0, 1.0},
+    };
+
+/// Directions of refinement of the cube.
+static const arma_mat DIRECTIONS = {
+      {-1.0, -1.0},
+      {-1.0, 1.0},
+      {1.0, -1.0},
+      {1.0, 1.0} 
+};
+
+} // CubeSphere::
 
 #endif  // INCLUDE_CUBESPHERE_H_
--- src/cubesphere.cpp
+++ src/cubesphere.cpp
@@ -1,24 +1,58 @@
 #include "aether.h"
+#include <cmath>
 
-arma_cube map_cube2sphere(const arma_cube& cube) {
-    arma_cube sphere(arma::size(cube));
-
-    sphere.slice(0) = cube2sphere(cube.slice(0), cube.slice(1), cube.slice(2));
-    sphere.slice(1) = cube2sphere(cube.slice(1), cube.slice(2), cube.slice(1));
-    sphere.slice(2) = cube2sphere(cube.slice(2), cube.slice(0), cube.slice(1));
-
-    return sphere;
-    }
-
-arma_vec cube2sphere(const arma_vec& x, const arma_vec& y, const arma_vec& z) {
+arma_vec CubeSphere::cube2sphere(const arma_vec& x, const arma_vec& y, const arma_vec& z) {
     const arma_vec y2_2 = (y%y) / 2;
     const arma_vec z2_2 = (y%y) / 2;
     const arma_vec one = arma::ones<arma_vec>(x.size());
     return x%(arma::sqrt(one-y2_2-z2_2+y2_2%z2_2*(2/3)));
-}
+    }
 
-precision_t cube2sphere_float(const precision_t x, const precision_t y, const precision_t z) {
+precision_t CubeSphere::cube2sphere(const precision_t& x, const precision_t& y, const precision_t& z) {
     const precision_t y2_2 = y*y/2;
     const precision_t z2_2 = z*z/2;
     return x*(sqrt(1-y2_2-z2_2+y2_2*z2_2*(2/3)));
+    }
+
+arma_vec CubeSphere::cube2sphere(const arma_vec& p){
+    const arma_vec p2 = p%p;
+    arma_vec ret_val = {0, 0, 0};
+    static const arma_mat others = {
+        {0, 1, 1},
+        {1, 0, 1},
+        {0, 1, 1}
+        };
+    const arma_mat last_term = {
+        {0, 0, p2(1)},
+        {p2(2), 0, 0},
+        {0, p2(0), 0}
+        };
+    const arma_vec one = arma::ones<arma_vec>(3);
+    ret_val = p%arma::sqrt(one-0.5*others*p2+last_term/3.0);
+
+    /* ret_val(0) = p(0)*sqrt(1.0-0.5*(p2(1)+p2(2))+(1.0/3.0)*(p2(1)*p2(2))); */
+    /* ret_val(1) = p(1)*sqrt(1.0-0.5*(p2(2)+p2(0))+(1.0/3.0)*(p2(2)*p2(0))); */
+    /* ret_val(2) = p(2)*sqrt(1.0-0.5*(p2(0)+p2(1))+(1.0/3.0)*(p2(0)*p2(1))); */
+
+    return ret_val;
+    }
+
+CubeSphere::QTNode::QTNode(const uint& face,
+                           const arma_vec& position,
+                           const uint& resolution,
+                           const uint& depth){
+    m_position = position;
+    m_depth = depth;
+
+    if (resolution != depth) {
+        // m_children = std::make_shared<QTNode>(position-
+        }
+    }
+
+CubeSphere::QTNode::~QTNode(){
+    // Place holder just in case.
+}
+
+bool CubeSphere::QTNode::is_leaf(){
+    return m_resolution == m_depth;
 }
