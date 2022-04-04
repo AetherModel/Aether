6b3bc4325fb1bacda214a43050a17bf80bcfa646
Ionospheres <meghan.burleigh@nrl.navy.mil>
2022-04-04 09:35:59 -0400

    Updated syntax to function outside Ion class

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -9,10 +9,9 @@
 // read in from the CSV file.
 // -----------------------------------------------------------------------------
 
-// removed this from the ions class // MB
 void calc_ion_neutral_coll_freq(Neutrals &neutrals, Ions &ions, Report &report) {
 
-  std::string function = "Ions::calc_ion_neutral_coll_freq";
+  std::string function = "calc_ion_neutral_coll_freq";
   static int iFunction = -1;
   report.enter(function, iFunction);
   arma_cube t, one_minus_log;
@@ -20,23 +19,23 @@ void calc_ion_neutral_coll_freq(Neutrals &neutrals, Ions &ions, Report &report) 
   for (int iIon = 0; iIon < nIons; iIon++) {
     if (ions.species[iIon].nu_ion_neutral_coef.size() > 0) {
       for (int iNeutral = 0; iNeutral < nSpecies; iNeutral++) {
-        species[iIon].nu_ion_neutral_vcgc[iNeutral].zeros();
+        ions.species[iIon].nu_ion_neutral_vcgc[iNeutral].zeros();
 
-        if (species[iIon].nu_is_resonant[iNeutral]) {
-          t = (species[iIon].nu_in_res_tn_frac[iNeutral] *
+        if (ions.species[iIon].nu_is_resonant[iNeutral]) {
+          t = (ions.species[iIon].nu_in_res_tn_frac[iNeutral] *
                neutrals.temperature_scgc +
-               species[iIon].nu_in_res_tn_frac[iNeutral] *
-               ion_temperature_scgc);
+               ions.species[iIon].nu_in_res_tn_frac[iNeutral] *
+               ions.ion_temperature_scgc);
           one_minus_log =
-            (1.0 - species[iIon].nu_in_res_coef2[iNeutral] *
+            (1.0 - ions.species[iIon].nu_in_res_coef2[iNeutral] *
              log10(t));
-          species[iIon].nu_ion_neutral_vcgc[iNeutral] =
-            species[iIon].nu_in_res_coef1[iNeutral] *
+          ions.species[iIon].nu_ion_neutral_vcgc[iNeutral] =
+            ions.species[iIon].nu_in_res_coef1[iNeutral] *
             neutrals.species[iNeutral].density_scgc %
             sqrt(t) % one_minus_log % one_minus_log;
         } else {
-          species[iIon].nu_ion_neutral_vcgc[iNeutral] =
-            species[iIon].nu_ion_neutral_coef[iNeutral] *
+          ions.species[iIon].nu_ion_neutral_vcgc[iNeutral] =
+            ions.species[iIon].nu_ion_neutral_coef[iNeutral] *
             neutrals.species[iNeutral].density_scgc;
         }
       }
