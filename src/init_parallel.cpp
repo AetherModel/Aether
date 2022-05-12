630d0c1a931ddda7325a27a4b1afe03a53c93ebd
Aaron Ridley <ridley@umich.edu>
2022-05-12 19:48:25 -0400

    FEAT: Modify num of blocks based on num or procs

--- src/init_parallel.cpp
+++ src/init_parallel.cpp
@@ -24,6 +24,7 @@ std::string cGrid;
 MPI_Comm aether_comm;
 
 bool init_parallel(Inputs &input,
+		   Quadtree &quadtree,
                    Report &report) {
 
   bool DidWork = true;
@@ -45,11 +46,19 @@ bool init_parallel(Inputs &input,
 
   nMembers = input.get_nMembers();
 
-  // Check to see if we have enough processors to do this stuff:
+  nGrids = nProcs / nMembers;
 
-  int nBlocksLonGeo = input.get_nBlocksLonGeo();
-  int nBlocksLatGeo = input.get_nBlocksLatGeo();
-  nGrids = nBlocksLonGeo * nBlocksLatGeo;
+  int64_t nProcsPerNode = nGrids / quadtree.nRootNodes;
+  if (report.test_verbose(2))
+    std::cout << "Number of PEs per root node available: " << nProcsPerNode << "\n";
+  quadtree.max_depth = log(nProcsPerNode) / log(4);
+  if (report.test_verbose(2))
+    std::cout << "Quadtree max depth : " << quadtree.max_depth << "\n";
+
+  // Check to see if we have enough processors to do this stuff:
+  int nBlocksLonGeo = pow(2, quadtree.max_depth); // input.get_nBlocksLonGeo();
+  int nBlocksLatGeo = pow(2, quadtree.max_depth); // input.get_nBlocksLatGeo();
+  nGrids = nBlocksLonGeo * nBlocksLatGeo * quadtree.nRootNodes;
   int nProcsNeeded = nMembers * nGrids;
 
   if (nProcsNeeded == nProcs) {
@@ -86,6 +95,8 @@ bool init_parallel(Inputs &input,
     if (report.test_verbose(2))
       std::cout << "seed : " << seed << "\n";
 
+    quadtree.build(input, report);
+    
   } else {
     if (iProc == 0) {
       std::cout << "Number of processors needed is not set correctly!\n";
