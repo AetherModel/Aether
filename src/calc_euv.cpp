cfe476b8f0af114adce00d19a928be4010374de5
garvp <garvp@umich.edu>
2023-05-10 15:33:14 -0400

    had to undo my changes for student starter project so I could merge into develop branch. forgot to switch to develop before I started actually developing

--- src/calc_euv.cpp
+++ src/calc_euv.cpp
@@ -32,7 +32,7 @@ int calc_euv(Planets planet,
 
     if (input.get_is_student())
       report.print(-1, "(2) What function is this " +
-		   input.get_student_name() + "this is calc euv ");
+		   input.get_student_name() +"?");
     
     // Chapman integrals for EUV energy deposition:
     neutrals.calc_chapman(grid, report);
