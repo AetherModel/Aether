89235c71c618830fb49c93ec94114026d2689645
Sudhan Annamalai <scannama@umich.edu>
2023-05-30 20:57:22 -0400

    BUG: fixed equation

--- src/solver_coriolis.cpp
+++ src/solver_coriolis.cpp
@@ -4,30 +4,38 @@
 
 
 // This function calculates the coriolis acceleration vector for an object 
-// in motion in a rotating frame of reference. It uses the following 
-// formula: a = 2 * W x v where W is the rotational velocity of the 
-// reference frame and v is the velocity of the object in the rotating frame 
-// This assumes the grid is lined up with the tilt 
+// in motion in a rotating frame of reference. It uses the coriolis terms in equations
+// (14), (18), (19) in https://drive.google.com/file/d/1Q0cMzKhdd0IoXzBl3odTge9JhBwfAyk9/view?usp=share_link
+
 #include "../include/aether.h"
 
-std::vector<arma_cube> coriolis(std::vector<arma_cube> velocity, precision_t rotation_rate) {
+std::vector<arma_cube> coriolis(std::vector<arma_cube> velocity, precision_t rotation_rate, arma_cube lat_scgc) {
     std::vector<arma_cube> coriolis_vec(3);
-    std::vector<arma_cube> cross;
+    // std::vector<arma_cube> cross;
 
     // Rotational velocity vector
-    std::vector<int> rotational_velocity{ 0, 0, rotation_rate};
+    // std::vector<int> rotational_velocity{ rotation_rate * cos(geoLon_scgc) * geoLat_scgc , 0, 0};
+    // std::vector<arma_cube> rotational_velocity(3);
+    // rotational_velocity[0].zeros();
+    // rotational_velocity[1].zeros();
+    // rotational_velocity[2].fill(rotation_rate);
 
+    coriolis_vec[0] = -2 * rotation_rate * velocity[1] % sin(lat_scgc);
+    coriolis_vec[1] = 2 * rotation_rate * velocity[0] % sin(lat_scgc) - 2 * rotation_rate * velocity[2] % cos(lat_scgc);
+    coriolis_vec[2] = 2 * rotation_rate * cos(lat_scgc) % velocity[1];
     // Cross Product of rotational velocity and velocity:
     // Latitude
-    cross.push_back(velocity[1] * rotational_velocity[2] - velocity[2] * rotational_velocity[1]);
+    // cross.push_back(velocity[1] * rotational_velocity[2] - velocity[2] * rotational_velocity[1]);
     // Longitude
-    cross.push_back(velocity[0] * rotational_velocity[2] - velocity[2] * rotational_velocity[0]);
+    // cross.push_back(velocity[0] * rotational_velocity[2] - velocity[2] * rotational_velocity[0]);
     // Altitude
-    cross.push_back(velocity[0] * rotational_velocity[1] - velocity[1] * rotational_velocity[0]);
+    // cross.push_back(velocity[0] * rotational_velocity[1] - velocity[1] * rotational_velocity[0]);
 
-    for (int64_t iComp; iComp < 3; iComp++) {
-        coriolis_vec[iComp] = 2 * cross[iComp];
-    }
+    
+    // for (int64_t iComp; iComp < 3; iComp++) {
+    //     coriolis_vec[iComp] = 2 * cross[iComp];
+        // coriolis_vec[iComp] = 2 * cross_product(velocity, rotational_velocity)
+    //}
 
     return coriolis_vec;
 }
