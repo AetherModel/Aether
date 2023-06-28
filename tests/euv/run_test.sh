bcf2d5cb4bd6e754ed557cc57f0d9c919ca686cf
Aaron Ridley <ridley@umich.edu>
2023-06-28 19:49:26 -0400

    FEAT: added photo electron test

--- tests/euv/run_test.sh
+++ tests/euv/run_test.sh
@@ -4,10 +4,23 @@
 rm -rf run.*
 
 # run 59 wavelengths, neuvac
+cp -R ../../share/run ./run.59_neuvac_nopei
+cd run.59_neuvac_nopei
+cp ../aether.json.59_neuvac_nopei ./aether.json
+mpirun -np 4 ./aether
+cd UA/output
+../../../../../srcPython/postAether.py -rm
+# This assumes aetherpy is installed and the plotter is in the bin directory:
+~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
+~/bin/run_plot_block_model_results.py -var=O2+ -alt=10 3DALL_20110320_003000.nc
+# into test directory
+cd ../../..
+
+# run 59 wavelengths, neuvac
 cp -R ../../share/run ./run.59_neuvac
 cd run.59_neuvac
 cp ../aether.json.59_neuvac ./aether.json
-mpirun -np 1 ./aether
+mpirun -np 4 ./aether
 cd UA/output
 ../../../../../srcPython/postAether.py -rm
 # This assumes aetherpy is installed and the plotter is in the bin directory:
@@ -20,7 +33,7 @@ cd ../../..
 cp -R ../../share/run ./run.59_euvac
 cd run.59_euvac
 cp ../aether.json.59_euvac ./aether.json
-mpirun -np 1 ./aether
+mpirun -np 4 ./aether
 cd UA/output
 ../../../../../srcPython/postAether.py -rm
 # This assumes aetherpy is installed and the plotter is in the bin directory:
@@ -33,7 +46,7 @@ cd ../../..
 cp -R ../../share/run ./run.37_euvac
 cd run.37_euvac
 cp ../aether.json.37_euvac ./aether.json
-mpirun -np 1 ./aether
+mpirun -np 4 ./aether
 cd UA/output
 ../../../../../srcPython/postAether.py -rm
 # This assumes aetherpy is installed and the plotter is in the bin directory:
