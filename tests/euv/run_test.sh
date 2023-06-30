7087accbd136b4b6945e573f602c1e289779ee21
Aaron Ridley <ridley@umich.edu>
2023-06-30 16:58:03 -0400

    FEAT: 22-bins test for EUVAC and HFG models

--- tests/euv/run_test.sh
+++ tests/euv/run_test.sh
@@ -3,6 +3,35 @@
 # remove old directories:
 rm -rf run.*
 
+# run 22 wavelengths, euvac
+cp -R ../../share/run ./run.22_euvac
+cd run.22_euvac
+cp ../aether.json.22_euvac ./aether.json
+mpirun -np 4 ./aether
+cd UA/output
+../../../../../srcPython/postAether.py -rm
+# This assumes aetherpy is installed and the plotter is in the bin directory:
+~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
+~/bin/run_plot_block_model_results.py -var=O2+ -alt=3 3DALL_20110320_003000.nc
+# into test directory
+cd ../../..
+
+# run 22 wavelengths, hfg solomon model
+cp -R ../../share/run ./run.22_hfg
+cd run.22_hfg
+cp ../aether.json.22_hfg ./aether.json
+mpirun -np 4 ./aether
+cd UA/output
+../../../../../srcPython/postAether.py -rm
+# This assumes aetherpy is installed and the plotter is in the bin directory:
+~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
+~/bin/run_plot_block_model_results.py -var=O2+ -alt=3 3DALL_20110320_003000.nc
+# into test directory
+cd ../../..
+
+exit
+
+
 # run 59 wavelengths, neuvac
 cp -R ../../share/run ./run.59_neuvac_nopei
 cd run.59_neuvac_nopei
