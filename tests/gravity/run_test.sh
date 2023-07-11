8a4b7915e3a1cbb627fb2339478013a6bbe292c2
Aaron Ridley <ridley@umich.edu>
2023-07-11 16:40:23 -0400

    FEAT: moved the output directory

--- tests/gravity/run_test.sh
+++ tests/gravity/run_test.sh
@@ -14,13 +14,15 @@ cd UA/output
 ../../../../../srcPython/postAether.py -rm
 pwd
 
-~/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
-~/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
-~/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Gvertical 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Gnorth 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Geast 3DGRA_20110320_000000.nc
 pwd
-mv 3DGRA_20110320_000000_Galtitude.png  ../../../../output_pngs
-mv 3DGRA_20110320_000000_Glatitude.png ../../../../output_pngs
-mv 3DGRA_20110320_000000_Glongitude.png ../../../../output_pngs
+mv 3DGRA_20110320_000000_Gvertical.png  ../../../output_pngs
+mv 3DGRA_20110320_000000_Gnorth.png ../../../output_pngs
+mv 3DGRA_20110320_000000_Geast.png ../../../output_pngs
+
+rm -rf *png *.bin *.json *.nc
 
 cd ../..
 
@@ -33,13 +35,15 @@ cd UA/output
 ../../../../../srcPython/postAether.py -rm
 pwd
 
-~/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
-~/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
-~/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Gvertical 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Gnorth 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Geast 3DGRA_20110320_000000.nc
 
-mv 3DGRA_20110320_000000_Galtitude.png ../../../../output_pngs/Oblate_Galtitude.png
-mv 3DGRA_20110320_000000_Glatitude.png ../../../../output_pngs/Oblate_Glatitude.png
-mv 3DGRA_20110320_000000_Glongitude.png ../../../../output_pngs/Oblate_Glongitude.png
+mv 3DGRA_20110320_000000_Gvertical.png ../../../output_pngs/Oblate_Gvertical.png
+mv 3DGRA_20110320_000000_Gnorth.png ../../../output_pngs/Oblate_Gnorth.png
+mv 3DGRA_20110320_000000_Geast.png ../../../output_pngs/Oblate_Geast.png
+
+rm -rf *png *.bin *.json *.nc
 
 cd ../..
 
@@ -51,10 +55,12 @@ cd UA/output
 
 ../../../../../srcPython/postAether.py -rm
 
-~/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
-~/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
-~/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Gvertical 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Gnorth 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Geast 3DGRA_20110320_000000.nc
 
-mv 3DGRA_20110320_000000_Galtitude.png ../../../../output_pngs/Oblate_withJ2_Galtitude.png
-mv 3DGRA_20110320_000000_Glatitude.png ../../../../output_pngs/Oblate_withJ2_Glatitude.png
-mv 3DGRA_20110320_000000_Glongitude.png ../../../../output_pngs/Oblate_withJ2_Glongitude.png
+mv 3DGRA_20110320_000000_Gvertical.png ../../../output_pngs/Oblate_withJ2_Gvertical.png
+mv 3DGRA_20110320_000000_Gnorth.png ../../../output_pngs/Oblate_withJ2_Gnorth.png
+mv 3DGRA_20110320_000000_Geast.png ../../../output_pngs/Oblate_withJ2_Geast.png
+
+rm -rf *png *.bin *.json *.nc
