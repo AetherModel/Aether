198579e6d47653b75dd01f3fa1db23ab655608ff
Aaron Ridley <ridley@umich.edu>
2023-07-11 16:20:02 -0400

    FEAT: assume plotting software is in ~/bin

--- tests/gravity/run_test.sh
+++ tests/gravity/run_test.sh
@@ -14,9 +14,9 @@ cd UA/output
 ../../../../../srcPython/postAether.py -rm
 pwd
 
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
 pwd
 mv 3DGRA_20110320_000000_Galtitude.png  ../../../../output_pngs
 mv 3DGRA_20110320_000000_Glatitude.png ../../../../output_pngs
@@ -33,9 +33,9 @@ cd UA/output
 ../../../../../srcPython/postAether.py -rm
 pwd
 
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
 
 mv 3DGRA_20110320_000000_Galtitude.png ../../../../output_pngs/Oblate_Galtitude.png
 mv 3DGRA_20110320_000000_Glatitude.png ../../../../output_pngs/Oblate_Glatitude.png
@@ -51,9 +51,9 @@ cd UA/output
 
 ../../../../../srcPython/postAether.py -rm
 
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
-../../../../../srcPython/aetherpy/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Galtitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Glatitude 3DGRA_20110320_000000.nc
+~/bin/run_plot_block_model_results.py -var=Glongitude 3DGRA_20110320_000000.nc
 
 mv 3DGRA_20110320_000000_Galtitude.png ../../../../output_pngs/Oblate_withJ2_Galtitude.png
 mv 3DGRA_20110320_000000_Glatitude.png ../../../../output_pngs/Oblate_withJ2_Glatitude.png
