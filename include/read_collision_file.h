bd7c832517a7515d2af5f703899752d21b4bc2d1
Aaron Ridley <ridley@umich.edu>
2023-07-12 15:36:39 -0400

    STY: spacing

--- include/read_collision_file.h
+++ include/read_collision_file.h
@@ -1,8 +1,8 @@
 // Copyright 2020, the Aether Development Team (see doc/dev_team.md for members)
 // Full license can be found in License.md
 
-#ifndef INCLUDE_COLLISION_H_
-#define INCLUDE_COLLISION_H_
+#ifndef INCLUDE_COLLISION_FILE_H_
+#define INCLUDE_COLLISION_FILE_H_
 
 #include "../include/aether.h"
 
@@ -22,15 +22,15 @@ void parse_resonant_nu_in_table(std::vector<std::vector<std::string>> csv,
 				Report &report);
 
 void parse_bst_in_table(std::vector<std::vector<std::string>> csv,
-				Neutrals &neutrals,
-				Ions &ions,
-				Report &report);
+			Neutrals &neutrals,
+			Ions &ions,
+			Report &report);
 
 
 void parse_diffexp_in_table(std::vector<std::vector<std::string>> csv,
-                          Neutrals &neutrals,
-                          Ions &ions,
-                          Report &report);
+			    Neutrals &neutrals,
+			    Ions &ions,
+			    Report &report);
 
 void parse_diff0_in_table(std::vector<std::vector<std::string>> csv,
                           Neutrals &neutrals,
@@ -41,7 +41,4 @@ void check_collision_frequncies(Ions ions,
 				Neutrals neutrals,
 				Report &report);
 
-
-
-#endif  // INCLUDE_IONS_H_
-
+#endif  // INCLUDE_COLLISION_FILE_H_
