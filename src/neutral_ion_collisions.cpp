a44f357945f520a43ceb863797e3ef4e64f029a6
Aaron Ridley <ridley@umich.edu>
2023-07-13 20:38:07 -0400

    BUG: basically rewrote

--- src/neutral_ion_collisions.cpp
+++ src/neutral_ion_collisions.cpp
@@ -3,34 +3,41 @@
 
 #include "../include/aether.h"
 
-void Neutrals::calc_ion_collisions(Ions &ions, Grid &grid, precision_t dt, Report &report) {
+void calc_ion_collisions(Neutrals &neutrals,
+			 Ions &ions,
+			 Report &report) {
     
-    std::string function = "Neutrals::calc_ion_collisions";
-    static int iFunction = -1;
-    report.enter(function, iFunction);
+  std::string function = "calc_ion_collisions";
+  static int iFunction = -1;
+  report.enter(function, iFunction);
     
-    int64_t nX = grid.get_nX();
-    int64_t nY = grid.get_nY();
-    int64_t nZ = grid.get_nZ();
+  int64_t nX = neutrals.density_scgc.n_rows;
+  int64_t nY = neutrals.density_scgc.n_cols;
+  int64_t nZ = neutrals.density_scgc.n_slices;
+  int64_t nSpecies = neutrals.nSpecies, iSpecies;
+  int64_t iDir, iIon, iNeutral;
     
-    arma_cube rho_n(nX, nY, nZ);
-    arma_cube rho_i(nX, nY, nZ);
-    arma_cube rho_sum(nX, nY, nZ);
+  arma_cube rho_n(nX, nY, nZ);
+  arma_cube rho_i(nX, nY, nZ);
+  arma_cube rho_sum(nX, nY, nZ);
     
-    // Calculate acceleration due to ion drag. Based on Formula 4.124b in Ionospheres text.
-    for (int64_t iNeutral = 0; iNeutral < nSpecies; iNeutral++) {
-        rho_n = species[iNeutral].mass * species[iNeutral].density_scgc;
-        
-        for (int64_t iDir = 0; iDir < 3; iDir++) {
-            for (int64_t iIon = 0; iIon < ions.nSpecies; iIon++) {
-                rho_i = ion.species[iIon].mass * ion.species[iIon].density_scgc;
-                rho_sum += rho_i % ions.species[iIon].nu_ion_neutral_coef[iSpecies]
-                    % (ions.species[iIon].par_velocity_vcgc[iDir] + ions.species[iIon].perp_velocity_vcgc[iDir] - species[iNeutral].velocity_vcgc[iDir]);
-            } // for each ion
-            species[iNeutral].acc_ion_drag[iDir] = rho_sum / rho_n;
-        } // for each direction
-    } // for each neutral
+  // Calculate acceleration due to ion drag. Based on Formula 4.124b in Ionospheres text.
+  for (iNeutral = 0; iNeutral < nSpecies; iNeutral++) {
+    rho_n = neutrals.species[iNeutral].mass * neutrals.species[iNeutral].density_scgc;
+    for (iDir = 0; iDir < 3; iDir++) {
+      rho_sum.zeros();
+      for (iIon = 0; iIon < ions.nSpecies; iIon++) {
+	rho_i = ions.species[iIon].mass * ions.species[iIon].density_scgc;
+	rho_sum = rho_sum +
+	  rho_i % ions.species[iIon].nu_ion_neutral_vcgc[iNeutral] %
+	  (ions.species[iIon].par_velocity_vcgc[iDir] +
+	   ions.species[iIon].perp_velocity_vcgc[iDir] -
+	   neutrals.species[iNeutral].velocity_vcgc[iDir]);
+      } // for each ion
+      neutrals.species[iNeutral].acc_ion_drag[iDir] = rho_sum / rho_n;
+    } // for each direction
+  } // for each neutral
     
-    report.exit(function);
+  report.exit(function);
     
 } // calc_ion_collisions
