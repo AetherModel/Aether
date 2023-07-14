a0d7eeebeb35164054e5a2d7c185970e8142e1c4
Aaron Ridley <ridley@umich.edu>
2023-07-13 20:26:48 -0400

    FEAT: don't need ions

--- include/read_collision_file.h
+++ include/read_collision_file.h
@@ -29,12 +29,10 @@ void parse_bst_in_table(std::vector<std::vector<std::string>> csv,
 
 void parse_diffexp_in_table(std::vector<std::vector<std::string>> csv,
 			    Neutrals &neutrals,
-			    Ions &ions,
 			    Report &report);
 
 void parse_diff0_in_table(std::vector<std::vector<std::string>> csv,
                           Neutrals &neutrals,
-                          Ions &ions,
                           Report &report);
 
 void check_collision_frequncies(Ions ions,
