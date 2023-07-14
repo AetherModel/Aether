8e8d968cfe10da326fe95b43e85a4221315b0aba
Aaron Ridley <ridley@umich.edu>
2023-07-13 20:27:59 -0400

    BUG: moved ion_drag here

--- include/calc_momentum_friction.h
+++ include/calc_momentum_friction.h
@@ -5,14 +5,24 @@
 #define INCLUDE_CALC_MOMENTUM_FRICTION_H_
 
 arma_vec neutral_friction_one_cell(int64_t iLong, int64_t iLat, int64_t iAlt,
-				   precision_t dt,
 				   arma_vec &vels,
 				   Neutrals &neutrals,
                                    Report &report);
 
-void calc_neutral_friction(precision_t dt,
-                           Grid &gGrid,
-                           Neutrals &neutrals,
+void calc_neutral_friction(Neutrals &neutrals,
                            Report &report);
+
+
+/**********************************************************************
+  \brief Calculate acceleration due to ion drag
+  \param ions The ions with which we are calculating drag
+  \param grid The grid to define the neutrals on
+  \param dt The change in time
+  \param report allow reporting to occur
+**/
+void calc_ion_collisions(Neutrals &neutrals,
+			 Ions &ions,
+			 Report &report);
+
 
 #endif  // INCLUDE_CALC_MOMENTUM_FRICTION_H_
