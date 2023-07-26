7f52c7ad54d07fddd3137f8b24b94273505b0474
Aaron Ridley <ridley@umich.edu>
2023-07-26 16:38:33 -0400

    STY: added return, removed spaces

--- src/neutral_ion_collisions.cpp
+++ src/neutral_ion_collisions.cpp
@@ -6,24 +6,27 @@
 void calc_ion_collisions(Neutrals &neutrals,
 			 Ions &ions,
 			 Report &report) {
-    
+
   std::string function = "calc_ion_collisions";
   static int iFunction = -1;
   report.enter(function, iFunction);
-    
+
   int64_t nX = neutrals.density_scgc.n_rows;
   int64_t nY = neutrals.density_scgc.n_cols;
   int64_t nZ = neutrals.density_scgc.n_slices;
   int64_t nSpecies = neutrals.nSpecies, iSpecies;
   int64_t iDir, iIon, iNeutral;
-    
+
   arma_cube rho_n(nX, nY, nZ);
   arma_cube rho_i(nX, nY, nZ);
   arma_cube rho_sum(nX, nY, nZ);
-    
-  // Calculate acceleration due to ion drag. Based on Formula 4.124b in Ionospheres text.
+
+  // Calculate acceleration due to ion drag. Based on Formula 4.124b
+  // in Ionospheres text.
   for (iNeutral = 0; iNeutral < nSpecies; iNeutral++) {
-    rho_n = neutrals.species[iNeutral].mass * neutrals.species[iNeutral].density_scgc;
+    rho_n =
+      neutrals.species[iNeutral].mass *
+      neutrals.species[iNeutral].density_scgc;
     for (iDir = 0; iDir < 3; iDir++) {
       rho_sum.zeros();
       for (iIon = 0; iIon < ions.nSpecies; iIon++) {
@@ -37,7 +40,7 @@ void calc_ion_collisions(Neutrals &neutrals,
       neutrals.species[iNeutral].acc_ion_drag[iDir] = rho_sum / rho_n;
     } // for each direction
   } // for each neutral
-    
+
   report.exit(function);
-    
+  return;
 } // calc_ion_collisions
