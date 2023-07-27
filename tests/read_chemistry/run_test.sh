a0e5b6b3b48a1d7e1a1405e0cd8fcd287723ed9e
Aaron Ridley <ridley@umich.edu>
2023-07-27 14:26:36 -0400

    BUG: had commented stuff out

--- tests/read_chemistry/run_test.sh
+++ tests/read_chemistry/run_test.sh
@@ -3,11 +3,11 @@
 # remove old directory:
 rm -rf run.no_errors run.errors
 
-## run without any errors to check it works:
-#cp -R ../../share/run ./run.no_errors
-#cd run.no_errors
-#./aether
-#cd ../
+# run without any errors to check it works:
+cp -R ../../share/run ./run.no_errors
+cd run.no_errors
+./aether
+cd ../
 
 #now we get into the real testing:
 cp -R ../../share/run ./run.errors
