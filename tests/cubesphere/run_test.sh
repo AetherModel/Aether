cef26d46c394fe5bcbff57516c43c0593866faad
Aaron Ridley <ridley@umich.edu>
2023-08-13 11:55:59 -0400

    FEAT: changed the aether.json file

--- tests/cubesphere/run_test.sh
+++ tests/cubesphere/run_test.sh
@@ -4,15 +4,15 @@ rm -rf run.cube
 
 cp -R ../../share/run ./run.cube
 cd run.cube
-cp ../../../share/examples/aether_cubesphere.json ./aether.json
+cp ../aether_cubesphere.json ./aether.json
 mpirun -np 6 ./aether
 # plot the output
 cd UA/output
 ../../../../../srcPython/postAether.py -rm
 # [O]:
-run_plot_block_model_results.py -var=O -alt=30 3DALL_20110320_001000.nc
+~/bin/run_plot_block_model_results.py -var=O -alt=30 3DALL_20110320_001000.nc
 # Tn:
-run_plot_block_model_results.py -var=Temperature -alt=30 3DALL_20110320_001000.nc
+~/bin/run_plot_block_model_results.py -var=Temperature -alt=30 3DALL_20110320_001000.nc
 # [e-]
-run_plot_block_model_results.py -var=e- -alt=5 3DALL_20110320_001000.nc
+~/bin/run_plot_block_model_results.py -var=e- -alt=5 3DALL_20110320_001000.nc
 
