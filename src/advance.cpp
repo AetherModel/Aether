c0b009ebaf636df150905a556f8477e4b2ccab42
Aaron Ridley <ridley@umich.edu>
2023-08-23 18:29:21 -0400

    BUG: things depend on mean major mass

--- src/advance.cpp
+++ src/advance.cpp
@@ -37,9 +37,10 @@ int advance(Planets &planet,
 
   gGrid.calc_sza(planet, time);
   neutrals.calc_mass_density();
+  neutrals.calc_mean_major_mass();
+  
   neutrals.calc_specific_heat();
   neutrals.calc_concentration();
-  neutrals.calc_mean_major_mass();
   neutrals.calc_pressure();
   neutrals.calc_bulk_velocity();
   neutrals.calc_kappa_eddy();
