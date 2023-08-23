e108ff5e16a058dd555cfdd305ebef2a85dad4a0
Aaron Ridley <ridley@umich.edu>
2023-08-23 18:39:06 -0400

    FEAT: only do this if altitude dependent

--- src/neutrals_momentum_friction.cpp
+++ src/neutrals_momentum_friction.cpp
@@ -96,24 +96,30 @@ void calc_neutral_friction(Neutrals &neutrals) {
         neutrals.species[iSpecies].acc_neutral_friction[iDir].zeros();
     } else {
 
-      for (iAlt = 0; iAlt < nZs; iAlt++) {
-        for (iLat = 0; iLat < nYs; iLat++) {
-          for (iLon = 0; iLon < nXs; iLon++) {
-            vels.zeros();
+      if (nZs > 1 + 2 * 2) {
+      
+	for (iAlt = 0; iAlt < nZs; iAlt++) {
+	  for (iLat = 0; iLat < nYs; iLat++) {
+	    for (iLon = 0; iLon < nXs; iLon++) {
+	      vels.zeros();
 
-            //Put the old velocities into vels:
-            for (iSpecies = 0; iSpecies < neutrals.nSpecies; iSpecies++)
-              vels(iSpecies) =
-                neutrals.species[iSpecies].velocity_vcgc[iDir](iLon, iLat, iAlt);
+	      //Put the old velocities into vels:
+	      for (iSpecies = 0; iSpecies < neutrals.nSpecies; iSpecies++)
+		vels(iSpecies) =
+		  neutrals.species[iSpecies].velocity_vcgc[iDir](iLon, iLat, iAlt);
 
-            acc = neutral_friction_one_cell(iLon, iLat, iAlt, vels, neutrals);
+	      acc = neutral_friction_one_cell(iLon, iLat, iAlt, vels, neutrals);
 
-            for (iSpecies = 0; iSpecies < neutrals.nSpecies; iSpecies++)
-              neutrals.species[iSpecies].acc_neutral_friction[iDir](iLon, iLat, iAlt) =
-                acc(iSpecies);
-          } // for long
-        } // for lat
-      } // for alt
+	      for (iSpecies = 0; iSpecies < neutrals.nSpecies; iSpecies++)
+		neutrals.species[iSpecies].acc_neutral_friction[iDir](iLon, iLat, iAlt) =
+		  acc(iSpecies);
+	    } // for long
+	  } // for lat
+	} // for alt
+      } else {
+	for (iSpecies = 0; iSpecies < neutrals.nSpecies; iSpecies++)
+	  neutrals.species[iSpecies].acc_neutral_friction[iDir].zeros();
+      } // else nZs
     } // if iDir
   } // for direction
 
