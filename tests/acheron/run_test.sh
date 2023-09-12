43f0fc56b9e20f5b6a47232c5df8ee12eab3a121
Aaron Ridley <ridley@umich.edu>
2023-09-11 20:57:20 -0400

    FEAT: test for acheron

---
+++ tests/acheron/run_test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -rf ./run.test_*
+
+# ----------------------------------------------------------------------
+# run the rusanov test:
+
+cp -R ../../share/run ./run.test_acheron
+cd run.test_acheron
+cp ../aether.json.acheron ./aether.json
+mpirun -np 6 ./aether
+
+# post process and plot:
+cd UA/output
+~/bin/postAether.py -rm
+# not sure where plotting code is located....
+~/bin/run_plot_block_model_results.py 3DALL_20110320_*.nc -var="Temperature" -alt=2
+
