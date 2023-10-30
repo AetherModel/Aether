1d7a8c0257b7c102c705341adf9e4de5fe159809
Aaron Ridley <ridley@umich.edu>
2023-10-30 19:54:55 -0400

    FEAT: passing string example

--- edu/examples/Fortran/print_hi.f90
+++ edu/examples/Fortran/print_hi.f90
@@ -7,13 +7,51 @@ module memories
 
 end module memories
 
+subroutine convert_int_to_string(inArray, outString, length)
+  implicit none
+  integer, intent(in) :: length
+  integer, dimension(length), intent(in) :: inArray
+  character(length), intent(out) :: outString
 
-subroutine print_hi() bind(C)
+  integer :: i
+  do i = 1, length
+    if (inArray(i) > 1) then
+      write(*,*) 'for : ', i, inArray(i)
+      outString(i:i) = achar(inArray(i))
+    else
+      outString(i:i) = achar(0)
+    endif
+  enddo
+
+end subroutine convert_int_to_string
+
+subroutine test_passing_string(intArray) bind(C, name = 'test_passing_string')
+  implicit none
+  integer, parameter :: iLength_ = 100
+  integer :: intArray(iLength_)
+  character(iLength_) :: charArray
+  integer :: i
+
+  call convert_int_to_string(intArray, charArray, iLength_)
+
+!  do i = 1, iLength_
+!    if (intArray(i) > 1) then
+!      write(*,*) 'for : ', i, intArray(i)
+!      charArray(i:i) = achar(intArray(i))
+!    else
+!      charArray(i:i) = ' '
+!    endif
+!  enddo
+
+  write(*,*) "string : ", charArray
+end subroutine test_passing_string
+
+subroutine print_hi() bind(C, name = 'print_hi')
   implicit none
   write(*,*) "Hello from Fortran."
 end subroutine print_hi
 
-subroutine print_double(i, x, y) bind(C)
+subroutine print_double(i, x, y) bind(C, name = 'print_double')
   implicit none
   integer :: i
   real :: x
@@ -23,7 +61,7 @@ subroutine print_double(i, x, y) bind(C)
   write(*,*) "Real in Fortran: ", x
 end subroutine print_double
 
-subroutine pass_arrays(array, twotimes) bind(C)
+subroutine pass_arrays(array, twotimes) bind(C, name = 'pass_arrays')
 
   use memories
   
@@ -38,7 +76,7 @@ subroutine pass_arrays(array, twotimes) bind(C)
   enddo
 end subroutine pass_arrays
 
-subroutine get_array(array_back) bind(C)
+subroutine get_array(array_back) bind(C, name = 'get_array')
 
   use memories
   
