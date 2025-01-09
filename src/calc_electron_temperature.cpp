a96d38f2ac47c0cab9e6ad87b0324bf00f479415
Aaron Bukowski <abukowski21@gmail.com>
2025-01-09 16:44:29 -0500

    FEAT: Get set up for doing e- temp

--- src/calc_electron_temperature.cpp
+++ src/calc_electron_temperature.cpp
@@ -9,8 +9,20 @@
 
 void Ions::calc_electron_temperature(Neutrals neutrals, Grid grid) {
 
+  std::string function = "Ions::calc_electron_temperature";
+  static int iFunction = -1;
+  report.enter(function, iFunction);
+
   electron_temperature_scgc = neutrals.temperature_scgc;
 
+  report.exit(function);
 }
 
-
+// --------------------------------------------------------------------------
+// Heating terms:
+//  - [ ] photoelectrons
+//  - [ ] auroral ionization
+//  - [ ] e- ion collisions
+//  - [ ] e- neutral collisions (elastic & inelastic)
+//  - [ ] e- chemistry (O2, V2 vibration; O fine structure, O exitation)
+// --------------------------------------------------------------------------
