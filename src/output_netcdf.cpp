e2caabe5c7da3a583a2417757b60b7c02d7edbee
Aaron James Ridley <ridley@umich.edu>
2025-02-28 10:29:49 -0500

    BUG: need / in path

--- src/output_netcdf.cpp
+++ src/output_netcdf.cpp
@@ -68,7 +68,7 @@ void output_netcdf_3d(std::vector<size_t> count_start,
 bool OutputContainer::read_container_netcdf() {
 
   bool didWork = true;
-  std::string whole_filename = directory  + filename + ".nc";
+  std::string whole_filename = directory + '/' + filename + ".nc";
   std::string UNITS = "units";
 
   try {
@@ -161,7 +161,7 @@ bool OutputContainer::read_container_netcdf() {
 bool OutputContainer::write_container_netcdf() {
 
   bool didWork = true;
-  std::string whole_filename = directory + filename + ".nc";
+  std::string whole_filename = directory + '/' + filename + ".nc";
   std::string UNITS = "units";
   std::string LONG_NAME = "long_name";
 
