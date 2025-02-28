64024f19a48cacd8f9f00ce55dd5831fe486fedb
Aaron Ridley <ridley@umich.edu>
2025-02-28 08:22:46 -0500

    FEAT: use python code for restarts

--- tests/restart_ensembles/run_all.sh
+++ tests/restart_ensembles/run_all.sh
@@ -1,38 +1,11 @@
 #!/bin/sh
 
-rm -rf run.test
-cp -R ../../share/run ./run.test
-cd run.test
+NPROC=5
+TOTALTIME=180
+MPI=/Users/ridley/Software/Mpi/installed/bin/mpirun
+NTIMES=2
 
-rm -f aether.json UA/output/*
-rm -rf UA/restart* ; mkdir UA/restartOut
-rm -rf UA/output_whole
-rm -rf UA/output_nonperturbed
-rm -rf UA/output_perturbed
-rm -rf UA/output_save
+# include -dowhole to run the whole simulation as comparison:
 
-cp ../aether.json.whole aether.json
-mpirun -np 20 ./aether
-cd UA/output ; ../../../../../srcPython/plot_logfiles.py -vars=10 log_m00*.txt ; cd -
-cd UA ; mv output output_whole ; mkdir output ; cd -
-rm -f aether.json
-
-rm -f aether.json UA/output/* UA/restartOut/*
-cp ../aether.json.start aether.json
-mpirun -np 20 ./aether
-cd UA ; mv restartOut restartIn ; mkdir restartOut ; cd -
-cd UA ; cp -R output output_save ; cd ..
-
-rm -f aether.json
-cp ../aether.json.restart1 aether.json
-mpirun -np 20 ./aether
-cd UA/output ; ../../../../../srcPython/plot_logfiles.py -vars=10 log_m00*.txt ; cd -
-cd UA ; mv output output_nonperturbed ; cp -R output_save output ; cd -
-
-rm -f aether.json
-cp ../aether.json.restart2 aether.json
-mpirun -np 20 ./aether
-cd UA/output ; ../../../../../srcPython/plot_logfiles.py -vars=10 log_m00*.txt ; cd -
-cd UA ; mv output output_perturbed ; mkdir output ; cd -
-
+../../srcPython/run_restarts.py -totaltime=${TOTALTIME} -mpi=${MPI} -rundir=../../share/run -ensembles=${NPROC} -restarts=${NTIMES}
 
