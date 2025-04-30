12ba82c8f08778739bb1d96965737b06c0bc0f2d
Aaron Ridley <ridley@umich.edu>
2025-04-30 10:08:31 -0400

    FEAT: exchange messages for ion grid

--- src/advance.cpp
+++ src/advance.cpp
@@ -127,6 +127,7 @@ bool advance(Planets &planet,
   if (gGrid.get_HasXdim() || gGrid.get_HasYdim()) {
     neutrals.exchange_old(gGrid);
     ions.exchange_old(gGrid);
+    ionsMag.exchange_old(mGrid);
     advect(gGrid, time, neutrals);
   }
 
