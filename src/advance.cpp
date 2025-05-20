b365ed045fd52c6482f79156b4a3f13812209848
Aaron Ridley <ridley@umich.edu>
2025-05-20 19:59:30 -0400

    FEAT: testing grid to grid MP

--- src/advance.cpp
+++ src/advance.cpp
@@ -46,6 +46,11 @@ bool advance(Planets &planet,
     didWork = neutralsMag.check_for_nonfinites("Top of Advance - ion grid");
   }
 
+  // here we are going to grab stuff from the neutral grid and put it on the
+  // ion grid
+  didWork = get_data_from_other_grid(gGrid, mGrid, neutrals.temperature_scgc, mGrid.test_scgc);
+
+
   gGrid.calc_sza(planet, time);
   mGrid.calc_sza(planet, time);
 
