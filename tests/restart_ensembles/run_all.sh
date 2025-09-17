be2a6b15b577885e21c9d7e220cca8146b0022ea
Aaron Ridley <ridley@umich.edu>
2025-09-16 20:16:36 -0400

    get to work with cubesphere

--- tests/restart_ensembles/run_all.sh
+++ tests/restart_ensembles/run_all.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-NPROC=5
+# cubesphere has 6 blocks:
+NBLOCKS=6
+# run with 3 members:
+NMEMBERS=2
+# run for a total of 180s
 TOTALTIME=180
+# this is the mpi command
 MPI=/usr/bin/mpirun
+# stop this many times
 NTIMES=2
 
 # include -dowhole to run the whole simulation as comparison:
 
-../../srcPython/run_restarts.py -totaltime=${TOTALTIME} -mpi=${MPI} -rundir=../../share/run -ensembles=${NPROC} -restarts=${NTIMES}
+../../srcPython/run_restarts.py -totaltime=${TOTALTIME} -mpi=${MPI} -rundir=../../share/run -ensembles=${NMEMBERS} -restarts=${NTIMES} -blocks=${NBLOCKS}
 
