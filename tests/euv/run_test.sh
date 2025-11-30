3a00aafc1d0a4d6b53af3759a9b27f96db5be55f
Aaron Ridley <ridley@umich.edu>
2025-11-30 07:23:33 -0500

    FEAT: plot a log file

--- tests/euv/run_test.sh
+++ tests/euv/run_test.sh
@@ -1,88 +1,136 @@
 #!/bin/sh
 
+PLOTTER=~/Software/PyITM/bin/plot_alt_wpolar.py
+
 # remove old directories:
 rm -rf run.*
 
+# run 59 wavelengths, FISM
+RUN=59_fism
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
+mpirun -np 4 ./aether
+cd UA/output
+../../../../../srcPython/postAether.py -rm
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
+# into test directory
+cd ../../..
+
+# run 59 wavelengths, FISM
+RUN=59_fism_nopei
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
+mpirun -np 4 ./aether
+cd UA/output
+../../../../../srcPython/postAether.py -rm
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
+# into test directory
+cd ../../..
+
+# run 59 wavelengths, neuvac
+RUN=59_neuvac
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
+mpirun -np 4 ./aether
+cd UA/output
+../../../../../srcPython/postAether.py -rm
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
+# into test directory
+cd ../../.. 
+
+# run 59 wavelengt, neuvac
+RUN=59_neuvac_nopei
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
+mpirun -np 4 ./aether
+cd UA/output
+../../../../../srcPython/postAether.py -rm
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
+# into test directory
+cd ../../.. 
+
+# run 59 wavelengths, neuvac
+RUN=59_euvac
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
+mpirun -np 4 ./aether
+cd UA/output
+../../../../../srcPython/postAether.py -rm
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
+# into test directory
+cd ../../.. 
+
 # run 22 wavelengths, euvac
-cp -R ../../share/run ./run.22_euvac
-cd run.22_euvac
-cp ../aether.json.22_euvac ./aether.json
+RUN=22_euvac
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
 mpirun -np 4 ./aether
 cd UA/output
 ../../../../../srcPython/postAether.py -rm
-# This assumes aetherpy is installed and the plotter is in the bin directory:
-~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
-~/bin/run_plot_block_model_results.py -var=O2+ -alt=3 3DALL_20110320_003000.nc
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
 # into test directory
-cd ../../..
+cd ../../.. 
 
-# run 22 wavelengths, hfg solomon model
-cp -R ../../share/run ./run.22_hfg
-cd run.22_hfg
-cp ../aether.json.22_hfg ./aether.json
+# run 22 wavelengths, HFG
+RUN=22_hfg
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
 mpirun -np 4 ./aether
 cd UA/output
 ../../../../../srcPython/postAether.py -rm
-# This assumes aetherpy is installed and the plotter is in the bin directory:
-~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
-~/bin/run_plot_block_model_results.py -var=O2+ -alt=3 3DALL_20110320_003000.nc
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
 # into test directory
-cd ../../..
-
-exit
-
-
-# run 59 wavelengths, neuvac
-cp -R ../../share/run ./run.59_neuvac_nopei
-cd run.59_neuvac_nopei
-cp ../aether.json.59_neuvac_nopei ./aether.json
-mpirun -np 4 ./aether
-cd UA/output
-../../../../../srcPython/postAether.py -rm
-# This assumes aetherpy is installed and the plotter is in the bin directory:
-~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
-~/bin/run_plot_block_model_results.py -var=O2+ -alt=3 3DALL_20110320_003000.nc
-# into test directory
-cd ../../..
-
-# run 59 wavelengths, neuvac
-cp -R ../../share/run ./run.59_neuvac
-cd run.59_neuvac
-cp ../aether.json.59_neuvac ./aether.json
-mpirun -np 4 ./aether
-cd UA/output
-../../../../../srcPython/postAether.py -rm
-# This assumes aetherpy is installed and the plotter is in the bin directory:
-~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
-~/bin/run_plot_block_model_results.py -var=O2+ -alt=3 3DALL_20110320_003000.nc
-# into test directory
-cd ../../..
+cd ../../.. 
 
 # run 59 wavelengths, euvac
-cp -R ../../share/run ./run.59_euvac
-cd run.59_euvac
-cp ../aether.json.59_euvac ./aether.json
+RUN=59_euvac
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
 mpirun -np 4 ./aether
 cd UA/output
 ../../../../../srcPython/postAether.py -rm
-# This assumes aetherpy is installed and the plotter is in the bin directory:
-~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
-~/bin/run_plot_block_model_results.py -var=O2+ -alt=3 3DALL_20110320_003000.nc
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
 # into test directory
-cd ../../..
+cd ../../.. 
 
 # run 37 wavelengths, euvac
-cp -R ../../share/run ./run.37_euvac
-cd run.37_euvac
-cp ../aether.json.37_euvac ./aether.json
+RUN=37_euvac
+cp -R ../../share/run ./run.${RUN}
+cd run.${RUN}
+cp ../aether.json.${RUN} ./aether.json
 mpirun -np 4 ./aether
 cd UA/output
 ../../../../../srcPython/postAether.py -rm
-# This assumes aetherpy is installed and the plotter is in the bin directory:
-~/bin/run_plot_block_model_results.py -var=Temperature -alt=20 3DALL_20110320_003000.nc
-~/bin/run_plot_block_model_results.py -var=O2+ -alt=3 3DALL_20110320_003000.nc
+# This assumes pyitm is installed and the plotter is in the bin directory:
+${PLOTTER} -var=Tn -alt=300 3DALG_20110320_001000.nc
+${PLOTTER} -var=O2+ -alt=120 3DALM_20110320_001000.nc
 # into test directory
 cd ../../.. 
 
 
+~/Software/PyITM/bin/plot_logfile.py run.*/UA/output/log_geo.txt -vars 17 10
 
